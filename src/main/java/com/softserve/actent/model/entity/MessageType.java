884c83a64d8cc53d52ab6cf81173d65262229e73
onlyVone <43787744+onlyVone@users.noreply.github.com>
2019-02-27 21:09:14 +0200

    Refactor code due to pull request comments

--- src/main/java/com/softserve/actent/model/entity/MessageType.java
+++ src/main/java/com/softserve/actent/model/entity/MessageType.java
@@ -3,13 +3,18 @@ package com.softserve.actent.model.entity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
-@Entity
-@Table(name = "messagetypes")
 @Data
 @NoArgsConstructor
+@Entity
+@Table(name = "messagetypes")
 public class MessageType {
 
     @Id
@@ -18,6 +23,6 @@ public class MessageType {
 
     @NonNull
     @NotBlank(message = "Review text can`t be empty.")
-    @Column(name = "type", unique = true)
+    @Column(unique = true)
     private String type;
 }
