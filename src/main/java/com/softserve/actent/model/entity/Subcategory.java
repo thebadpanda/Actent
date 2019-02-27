8e81cf5aea5937b5a23ad250c3c60dd2dbe75f1e
Barnabass <rastafarimood@gmail.com>
2019-02-27 21:09:26 +0200

    refactor Subcategory entity to

--- src/main/java/com/softserve/actent/model/entity/Subcategory.java
+++ src/main/java/com/softserve/actent/model/entity/Subcategory.java
@@ -3,8 +3,12 @@ package com.softserve.actent.model.entity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
-
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
 @Data
