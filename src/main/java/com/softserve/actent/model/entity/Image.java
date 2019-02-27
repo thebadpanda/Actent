f0c648c235e52e190fdafef4ecbd5ef79a851c86
Senchakmen <senchakevuch@gmail.com>
2019-02-27 20:47:55 +0200

    correct Image entity

--- src/main/java/com/softserve/actent/model/entity/Image.java
+++ src/main/java/com/softserve/actent/model/entity/Image.java
@@ -3,15 +3,13 @@ package com.softserve.actent.model.entity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.Length;
-
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
-@Table(name = "image")
+@Table(name = "images")
 @NoArgsConstructor
-@RequiredArgsConstructor
 @Data
 public class Image {
 
@@ -20,10 +18,13 @@ public class Image {
     private Long id;
 
     @NonNull
-    @Column(name = "file_path", unique = true)
+    @NotBlank(message = "File path can`t be empty.")
+    @Column(name = "file_path", unique = true, length = 256)
+    @Length(max = 256, message = "Too long")
     private String filePath;
 
     @NonNull
+    @NotBlank(message = "hash can`t be empty.")
     @Column(name = "hash", unique = true, length = 256)
     @Length(max = 256, message = "Too long")
     private String hash;
