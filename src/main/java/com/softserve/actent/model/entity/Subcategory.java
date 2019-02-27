a7022d5682c530cd7e0575ef2b4cb99c58d7e835
Barnabass <rastafarimood@gmail.com>
2019-02-27 21:49:11 +0200

    refactor Subcategory entity to

--- src/main/java/com/softserve/actent/model/entity/Subcategory.java
+++ src/main/java/com/softserve/actent/model/entity/Subcategory.java
@@ -3,6 +3,7 @@ package com.softserve.actent.model.entity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import org.hibernate.validator.constraints.Length;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -23,6 +24,7 @@ public class Subcategory {
 
     @NonNull
     @NotBlank(message = "Can't be empty")
+    @Length(max = 20, message = "Too long")
     @Column(unique = true, nullable = false, length = 20)
     private String type;
 }
