06ae3d230dc4e3e853ff1d4669be6dc6c06ee934
Arsen <arsenemarkiv@gmail.com>
2019-02-27 20:50:37 +0200

    created role entity

---
+++ src/main/java/com/softserve/actent/model/entity/Role.java
@@ -0,0 +1,26 @@
+package com.softserve.actent.model.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "roles")
+@Data
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "role", unique = true)
+    @NotBlank(message = "Role cannot be empty")
+    @NonNull
+    @Length(max = 20, message = "Too long")
+    private String role;
+}
