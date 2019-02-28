161e89c802511e4bf82756d94ac7602b5b9c4076
octap16 <octap17@gmail.com>
2019-02-28 20:37:04 +0200

    add annotation NonNull

--- src/main/java/com/softserve/actent/model/entity/Message.java
+++ src/main/java/com/softserve/actent/model/entity/Message.java
@@ -2,6 +2,7 @@ package com.softserve.actent.model.entity;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import javax.persistence.*;
 
@@ -16,12 +17,15 @@ public class Message {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NonNull
     @Enumerated(EnumType.ORDINAL)
     private MessageType messageType;
 
+    @NonNull
     @OneToOne(cascade = CascadeType.ALL)
     private MessageBody messageBody;
 
+    @NonNull
     @ManyToOne(fetch = FetchType.LAZY)
     private User sender;
 
