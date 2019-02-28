b76a72a4468cfe4441b8cf97e951d40c17bbf1a4
octap16 <octap17@gmail.com>
2019-02-28 19:49:03 +0200

    add relationship to MessageBody

--- src/main/java/com/softserve/actent/model/entity/Image.java
+++ src/main/java/com/softserve/actent/model/entity/Image.java
@@ -4,11 +4,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
@@ -33,4 +36,7 @@ public class Image {
     @Column(nullable = false, unique = true, length = 256)
     @Length(min = 256, max = 256, message = "Hash must be of exact length 256")
     private String hash;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private MessageBody messageBody;
 }
