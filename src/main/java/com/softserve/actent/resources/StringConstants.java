b9030236551c49a23951b4238748906846f3f2f6
Oleh Volchkov <volchkov.oleh@gmail.com>
2019-02-28 13:20:40 +0200

    created class for string constants

---
+++ src/main/java/com/softserve/actent/resources/StringConstants.java
@@ -0,0 +1,7 @@
+package com.softserve.actent.resources;
+
+public class StringConstants {
+
+    public static final String EMPTY_REWIEW_TEXT = "Review text can not be empty.";
+    public static final String NO_REVIEW_SCORE = "You must provide review score.";
+}
