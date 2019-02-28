709a521d668775a0cf682b9a62c8aec7c669fabe
Arsen <arsenemarkiv@gmail.com>
2019-02-28 21:44:48 +0200

    imports; unnecessary @column delete; list to set; other changes

--- src/main/java/com/softserve/actent/model/entity/User.java
+++ src/main/java/com/softserve/actent/model/entity/User.java
@@ -5,16 +5,30 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
 import java.util.List;
 
-@Entity
-@Table(name = "users")
+
 @NoArgsConstructor
 @Data
+@Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -66,7 +80,6 @@ public class User {
     private String bio;
 
     @NonNull
-    @Column
     @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "user_categories",
             joinColumns = {@JoinColumn(name = "user_id")},
@@ -78,16 +91,14 @@ public class User {
     @Column(nullable = false)
     private String sex;
 
-    @NonNull
-    @Column
-    @ManyToMany
-    @JoinTable(name = "user_events",
-            joinColumns = {@JoinColumn(name = "user_id")},
-            inverseJoinColumns = {@JoinColumn(name = "event_id")})
-    private List<Event> events;
+//    @NonNull
+//    @ManyToMany
+//    @JoinTable(name = "user_events",
+//            joinColumns = {@JoinColumn(name = "user_id")},
+//            inverseJoinColumns = {@JoinColumn(name = "event_id")})
+//    private Set<Event> events;
 
     @NonNull
-    @Column
     @OneToMany(fetch = FetchType.LAZY)
     @JoinColumn(name = "review_id", nullable = false)
     private List<Review> reviews;
