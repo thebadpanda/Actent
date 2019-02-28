dc8c233376a4c10fd67c604a6901a0c24dbf3965
onlyVone <43787744+onlyVone@users.noreply.github.com>
2019-02-28 13:19:31 +0200

    MessageType entity -> MessageType enum

--- src/main/java/com/softserve/actent/model/entity/MessageType.java
+++ src/main/java/com/softserve/actent/model/entity/MessageType.java
@@ -1,28 +1,7 @@
 package com.softserve.actent.model.entity;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.validation.constraints.NotBlank;
+public enum MessageType {
 
-@Data
-@NoArgsConstructor
-@Entity
-@Table(name = "messagetypes")
-public class MessageType {
+    EVENT_MESSAGE, DIRECT_MESSAGE
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @NonNull
-    @NotBlank(message = "Review text can`t be empty.")
-    @Column(unique = true, nullable = false)
-    private String type;
 }
