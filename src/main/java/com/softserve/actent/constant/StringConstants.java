d23256a2e354c2386c5721312bea6738d4e5964b
Oleh Volchkov <volchkov.oleh@gmail.com>
2019-02-28 17:27:23 +0200

    created tag entity

--- src/main/java/com/softserve/actent/constant/StringConstants.java
+++ src/main/java/com/softserve/actent/constant/StringConstants.java
@@ -4,4 +4,7 @@ public class StringConstants {
 
     public static final String EMPTY_REWIEW_TEXT = "Review text can not be empty.";
     public static final String NO_REVIEW_SCORE = "You must provide review score.";
+
+    public static final String NO_TAG_TEXT = "Tag text can not be empty.";
+
 }
