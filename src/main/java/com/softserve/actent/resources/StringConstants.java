cb41ed811ea2a4d6b150491754fe6ec19f2f3b7c
Ira Shturyn <ira.shturyn@gmail.com>
2019-02-28 15:31:05 +0200

    correct imports

--- src/main/java/com/softserve/actent/resources/StringConstants.java
+++ src/main/java/com/softserve/actent/resources/StringConstants.java
@@ -7,4 +7,6 @@ public class StringConstants {
     public static final String CITY_SHOULD_NOT_BE_BLANK = "City can`t be blank";
     public static final String REGION_SHOULD_NOT_BE_BLANK = "Region can`t be blank";
     public static final String COUNTRY_SHOULD_NOT_BE_BLANK ="Country can`t be blank";
+    public static final String TOO_LONG ="Too long";
+
 }
