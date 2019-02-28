36871c163a1b3e62db7ee872e22ec670e6f9e1e1
octap16 <octap17@gmail.com>
2019-02-28 19:44:24 +0200

    create message body entity

---
+++ src/main/java/com/softserve/actent/model/entity/MessageBody.java
@@ -0,0 +1,35 @@
+package com.softserve.actent.model.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "message_bodies")
+public class MessageBody {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    @Column(nullable = false)
+    private String text;
+
+    @OneToMany(mappedBy = "messageBody")
+    private Set<Image> images = new HashSet<>();
+}
