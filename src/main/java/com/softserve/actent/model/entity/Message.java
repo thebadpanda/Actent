1e954f4ec1762746e5969fb9c88b2a519b25a876
octap16 <octap17@gmail.com>
2019-02-28 20:40:24 +0200

    refactor imports

--- src/main/java/com/softserve/actent/model/entity/Message.java
+++ src/main/java/com/softserve/actent/model/entity/Message.java
@@ -4,7 +4,17 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 
 @Data
