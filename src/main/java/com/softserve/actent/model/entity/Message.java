a18ba74d6b6e1a7877901507a65eaa7577eb7cc5
octap16 <octap17@gmail.com>
2019-02-28 19:45:17 +0200

    create message entity

---
+++ src/main/java/com/softserve/actent/model/entity/Message.java
@@ -0,0 +1,36 @@
+package com.softserve.actent.model.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "messages")
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private MessageType messageType;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private MessageBody messageBody;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User sender;
+
+}
