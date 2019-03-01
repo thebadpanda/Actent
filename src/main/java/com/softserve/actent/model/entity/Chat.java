bf7d99bbf5c30f4454ecc0b1cbbb0866af11b634
onlyVone <43787744+onlyVone@users.noreply.github.com>
2019-03-01 12:18:10 +0200

    persistence...

--- src/main/java/com/softserve/actent/model/entity/Chat.java
+++ src/main/java/com/softserve/actent/model/entity/Chat.java
@@ -5,7 +5,17 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import java.util.HashSet;
 import java.util.Set;
