041a35a905a3e78f96720c737ec546deccda7131
onlyVone <43787744+onlyVone@users.noreply.github.com>
2019-03-01 12:21:19 +0200

    persistence...

--- src/main/java/com/softserve/actent/model/entity/Message.java
+++ src/main/java/com/softserve/actent/model/entity/Message.java
@@ -1,11 +1,21 @@
 package com.softserve.actent.model.entity;
 
-import com.sun.org.glassfish.gmbal.ManagedAttribute;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 
 @Data
