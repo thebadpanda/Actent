a20465269e8437c5f35dca2eaed44e6bbca2c66d
onlyVone <43787744+onlyVone@users.noreply.github.com>
2019-03-01 12:17:11 +0200

    Refactor Chat entity

--- src/main/java/com/softserve/actent/model/entity/Chat.java
+++ src/main/java/com/softserve/actent/model/entity/Chat.java
@@ -5,18 +5,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Data
@@ -32,16 +23,17 @@ public class Chat {
     @NonNull
     @OneToMany(fetch = FetchType.LAZY,
             mappedBy = "chat")
-    private List<Message> messages;
+    private Set<Message> messages;
 
     @NonNull
     @NotNull(message = StringConstants.CHAT_TYPE_NOT_BE_BLANK)
-    @Enumerated(EnumType.ORDINAL)
+    @Enumerated(EnumType.STRING)
     private ChatType type;
 
     @NonNull
-    @OneToMany(fetch=FetchType.LAZY,
-            mappedBy = "chat")
+    @Column(name = "banned_in_chat")
+    @ManyToMany(fetch=FetchType.LAZY,
+            mappedBy = "bannedChats")
     private Set<User> bannedUsers = new HashSet<>();
 
 }
