98bfe2d96fc1d307ddb53db7f9834914815fd6c7
onlyVone <43787744+onlyVone@users.noreply.github.com>
2019-03-01 12:19:22 +0200

    Add wiring to banned users in chats

--- src/main/java/com/softserve/actent/model/entity/Message.java
+++ src/main/java/com/softserve/actent/model/entity/Message.java
@@ -1,20 +1,11 @@
 package com.softserve.actent.model.entity;
 
+import com.sun.org.glassfish.gmbal.ManagedAttribute;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 
 @Data
@@ -38,5 +29,10 @@ public class Message {
     @NonNull
     @ManyToOne(fetch = FetchType.LAZY)
     private User sender;
+
+    @NonNull
+    @ManyToOne
+    @JoinColumn(name = "chat_id", nullable = false)
+    private Chat chat;
 
 }
