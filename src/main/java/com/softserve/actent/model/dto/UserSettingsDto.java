ed1a9004018a1e8d715ac171c598ee6a7450de34
Arsen <arsenemarkiv@gmail.com>
2019-03-05 13:55:17 +0200

    User start CRUD chain

---
+++ src/main/java/com/softserve/actent/model/dto/UserSettingsDto.java
@@ -0,0 +1,27 @@
+package com.softserve.actent.model.dto;
+
+import com.softserve.actent.model.entity.Image;
+import com.softserve.actent.model.entity.Location;
+import com.softserve.actent.model.entity.Role;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class UserSettingsDto {
+
+    private Long id;
+    private String login;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private LocalDate birthDate;
+    private Image avatar;
+    private Location location;
+    private String bio;
+    private String sex;
+    private Role role;
+
+
+}
--- src/main/java/com/softserve/actent/service/impl/UserServiceImpl.java
+++ src/main/java/com/softserve/actent/service/impl/UserServiceImpl.java
@@ -1,11 +1,15 @@
 package com.softserve.actent.service.impl;
 
+import com.softserve.actent.model.dto.UserSettingsDto;
+import com.softserve.actent.model.dto.RegisterUserDto;
 import com.softserve.actent.model.entity.User;
 import com.softserve.actent.repository.UserRepository;
 import com.softserve.actent.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -14,10 +18,59 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
 
-    @Override
-    public User addUser(User user) {
+    @Autowired
+    User user;
 
-        return userRepository.save(user);
+    @Transactional
+    @Override
+    public boolean registerUser(@RequestBody RegisterUserDto registerUserDto) {
+        boolean isSuccess;
+        if (!userRepository.existsById(registerUserDto.getId())) {
+            userRepository.save(userEntityToDto(registerUserDto));
+            isSuccess = true;
+        } else {
+            System.out.println("There is a user with such id");
+            isSuccess = false;
+        }
+        return isSuccess;
+    }
+
+    @Transactional
+    @Override
+    public boolean saveUserSettings(@RequestBody UserSettingsDto userSettingsDto) {
+        boolean isSuccess;
+        if (userRepository.existsById(userSettingsDto.getId())) {
+            userRepository.save(userSettingsToDto(userSettingsDto));
+            isSuccess = true;
+        } else {
+            isSuccess = false;
+        }
+        return isSuccess;
+    }
+
+    private User userEntityToDto(RegisterUserDto registerUserDto) {
+        User user = new User();
+        user.setEmail(registerUserDto.getEmail());
+        user.setLogin(registerUserDto.getLogin());
+        user.setFirstName(registerUserDto.getFirstName());
+        user.setLastName(registerUserDto.getLastName());
+        user.setPassword(registerUserDto.getPassword());
+        return user;
+    }
+
+    private User userSettingsToDto(UserSettingsDto userSettingsDto){
+        User user = new User();
+        user.setEmail(userSettingsDto.getEmail());
+        user.setLogin(userSettingsDto.getLogin());
+        user.setFirstName(userSettingsDto.getFirstName());
+        user.setLastName(userSettingsDto.getLastName());
+        user.setBirthDate(userSettingsDto.getBirthDate());
+        user.setAvatar(userSettingsDto.getAvatar());
+        user.setBio(userSettingsDto.getBio());
+        user.setSex(userSettingsDto.getSex());
+        user.setLocation(userSettingsDto.getLocation());
+        user.setRole(userSettingsDto.getRole());
+        return user;
     }
 
     @Override
