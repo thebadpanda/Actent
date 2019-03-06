dcd9454a0c104ef7316a757546fde0da2c911666
Oleh Volchkov <volchkov.oleh@gmail.com>
2019-03-06 17:39:41 +0200

    implemented endpoints

--- src/main/java/com/softserve/actent/controller/ImageController.java
+++ src/main/java/com/softserve/actent/controller/ImageController.java
@@ -1,7 +1,80 @@
 package com.softserve.actent.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.softserve.actent.model.dto.IdDto;
+import com.softserve.actent.model.dto.ImageDto;
+import com.softserve.actent.model.entity.Image;
+import com.softserve.actent.service.ImageService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1")
 public class ImageController {
+
+    private final ImageService imageService;
+
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public ImageController(ImageService imageService, ModelMapper modelMapper) {
+        this.imageService = imageService;
+        this.modelMapper = modelMapper;
+    }
+
+    @PostMapping(value = "/images")
+    public ResponseEntity<IdDto> addImage(@RequestBody ImageDto addImageDto) {
+
+        Image image = modelMapper.map(addImageDto, Image.class);
+        image = imageService.addImage(image);
+
+        return new ResponseEntity<>(new IdDto(image.getId()), HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/images/{id}")
+    public ResponseEntity<ImageDto> getImageById(@PathVariable Long id) {
+
+        Image image = imageService.getImageById(id);
+        return new ResponseEntity<>(modelMapper.map(image, ImageDto.class), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/images")
+    public ResponseEntity<?> getImages(@RequestParam(value = "url", required = false) String url) {
+
+        if (url != null) {
+
+            Image image = imageService.getImageByFilePath(url);
+
+            return new ResponseEntity<>(modelMapper.map(image, ImageDto.class), HttpStatus.OK);
+        } else {
+
+            List<Image> images = imageService.getAllImages();
+            List<ImageDto> imagesDto = new ArrayList<>();
+
+            for (Image image : images) {
+                imagesDto.add(modelMapper.map(image, ImageDto.class));
+            }
+
+            return new ResponseEntity<>(imagesDto, HttpStatus.OK);
+        }
+    }
+
+    @PutMapping(value = "/images/{id}")
+    public ResponseEntity<ImageDto> updateImage(@RequestBody ImageDto imageDto, @PathVariable Long id) {
+
+        Image image = imageService.updateImageById(modelMapper.map(imageDto, Image.class), id);
+        return new ResponseEntity<>(modelMapper.map(image, ImageDto.class), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/images/{id}")
+    public ResponseEntity<Void> deleteImage(@PathVariable Long id) {
+
+        imageService.deleteImageById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
