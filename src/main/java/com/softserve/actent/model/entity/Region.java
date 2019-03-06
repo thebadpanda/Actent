fd9ec8298292066128192ca5561964b2abdc5f39
Ira Shturyn <ira.shturyn@gmail.com>
2019-03-06 20:49:52 +0200

    add CascadeType to Country and Region

--- src/main/java/com/softserve/actent/model/entity/Region.java
+++ src/main/java/com/softserve/actent/model/entity/Region.java
@@ -1,11 +1,14 @@
 package com.softserve.actent.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.softserve.actent.constant.StringConstants;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import org.hibernate.validator.constraints.Length;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,8 +18,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -34,10 +36,12 @@ public class Region {
     private String name;
 
     @NonNull
+    @JsonManagedReference
     @ManyToOne
     private Country country;
 
     @NonNull
-    @OneToMany(mappedBy = "region")
-    private Set<City> cities = new HashSet<>();
+    @JsonBackReference
+    @OneToMany(mappedBy = "region", cascade = CascadeType.ALL)
+    private List<City> cities;
 }
