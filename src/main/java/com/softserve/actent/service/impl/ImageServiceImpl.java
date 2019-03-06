06d0860ecfa4f4253f93588ba7ccbf5f1abff0bd
Oleh Volchkov <volchkov.oleh@gmail.com>
2019-03-06 17:39:24 +0200

    added and implemented service methods

--- src/main/java/com/softserve/actent/service/impl/ImageServiceImpl.java
+++ src/main/java/com/softserve/actent/service/impl/ImageServiceImpl.java
@@ -1,8 +1,82 @@
 package com.softserve.actent.service.impl;
 
+import com.softserve.actent.model.entity.Image;
+import com.softserve.actent.repository.ImageRepository;
 import com.softserve.actent.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ImageServiceImpl implements ImageService {
+
+    private final ImageRepository imageRepository;
+
+    @Autowired
+    public ImageServiceImpl(ImageRepository imageRepository) {
+        this.imageRepository = imageRepository;
+    }
+
+    @Override
+    public Image addImage(Image image) {
+
+        return imageRepository.save(image);
+    }
+
+    @Override
+    public Image getImageById(Long imageId) {
+
+        Optional<Image> optionalImage = imageRepository.findById(imageId);
+
+        return optionalImage.orElse(null);
+    }
+
+    @Override
+    public Image getImageByFilePath(String filePath) {
+
+        Optional<Image> optionalImage = imageRepository.findByFilePath(filePath);
+
+        return optionalImage.orElse(null);
+    }
+
+    @Override
+    public Image getImageByHash(String hash) {
+
+        Optional<Image> optionalImage = imageRepository.findByHash(hash);
+
+        return optionalImage.orElse(null);
+    }
+
+    @Override
+    public List<Image> getAllImages() {
+
+        return imageRepository.findAll();
+    }
+
+    @Override
+    public Image updateImageById(Image image, Long imageId) {
+
+        Optional<Image> optionalImage = imageRepository.findById(imageId);
+
+        if (optionalImage.isPresent()) {
+            image.setId(imageId);
+            return imageRepository.save(image);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void deleteImageById(Long imageId) {
+
+        Optional<Image> optionalImage = imageRepository.findById(imageId);
+
+        if(optionalImage.isPresent()) {
+            imageRepository.deleteById(imageId);
+        }
+
+        // TODO: else throw exception or so
+    }
 }
