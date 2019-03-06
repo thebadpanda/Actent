3f664d09a9db22bc3f968c63d72a18b05f5cb98f
RBarnabass <rastafarimood@gmail.com>
2019-03-06 21:41:05 +0200

    annotation

--- src/main/java/com/softserve/actent/repository/ReviewRepository.java
+++ src/main/java/com/softserve/actent/repository/ReviewRepository.java
@@ -2,6 +2,8 @@ package com.softserve.actent.repository;
 
 import com.softserve.actent.model.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReviewRepository extends JpaRepository <Review, Long> {
 }
