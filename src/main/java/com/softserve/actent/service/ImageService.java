7bebc240f37744cc56e84e4b1201497101b3ae46
Oleh Volchkov <volchkov.oleh@gmail.com>
2019-03-06 13:08:41 +0200

    initialized image service and implementation

---
+++ src/main/java/com/softserve/actent/service/ImageService.java
@@ -0,0 +1,4 @@
+package com.softserve.actent.service;
+
+public interface ImageService {
+}
---
+++ src/main/java/com/softserve/actent/service/impl/ImageServiceImpl.java
@@ -0,0 +1,8 @@
+package com.softserve.actent.service.impl;
+
+import com.softserve.actent.service.ImageService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImageServiceImpl implements ImageService {
+}
