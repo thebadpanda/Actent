f58f588db29edb143f39ebdfcae04377451db068
onlyVone <43787744+onlyVone@users.noreply.github.com>
2019-03-06 17:37:00 +0200

    Refactor chat logic

--- src/main/java/com/softserve/actent/controller/ChatController.java
+++ src/main/java/com/softserve/actent/controller/ChatController.java
@@ -5,7 +5,12 @@ import com.softserve.actent.model.dto.IdDto;
 import com.softserve.actent.model.entity.Chat;
 import com.softserve.actent.service.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
