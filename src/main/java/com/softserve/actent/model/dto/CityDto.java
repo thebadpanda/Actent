40da1048e39cef033e6ded042bf110c462108dde
Ira Shturyn <ira.shturyn@gmail.com>
2019-03-07 00:41:24 +0200

    modified contry, region, city, location dto

--- src/main/java/com/softserve/actent/model/dto/CityDto.java
+++ src/main/java/com/softserve/actent/model/dto/CityDto.java
@@ -1,9 +1,10 @@
 package com.softserve.actent.model.dto;
 
 import lombok.Data;
+import lombok.NonNull;
 
 @Data
 public class CityDto {
+    @NonNull
     private String name;
-    private String regionName;
 }
--- src/main/java/com/softserve/actent/model/dto/CountryDto.java
+++ src/main/java/com/softserve/actent/model/dto/CountryDto.java
@@ -1,8 +1,10 @@
 package com.softserve.actent.model.dto;
 
 import lombok.Data;
+import lombok.NonNull;
 
 @Data
 public class CountryDto {
+    @NonNull
     private String name;
 }
