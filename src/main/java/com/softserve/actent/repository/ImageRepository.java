4473932206735b7cc4c5f07f1ed95cd9a80c7391
Oleh Volchkov <volchkov.oleh@gmail.com>
2019-03-06 17:38:55 +0200

    added repository methods

--- src/main/java/com/softserve/actent/repository/ImageRepository.java
+++ src/main/java/com/softserve/actent/repository/ImageRepository.java
@@ -4,6 +4,12 @@ import com.softserve.actent.model.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Long> {
+
+    Optional<Image> findByFilePath(String filePath);
+
+    Optional<Image> findByHash(String hash);
 }
