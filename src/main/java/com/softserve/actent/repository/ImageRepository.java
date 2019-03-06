55f73c1981916223d7979a9cb5b965996f77fd33
Oleh Volchkov <volchkov.oleh@gmail.com>
2019-03-06 13:09:42 +0200

    created image repository

---
+++ src/main/java/com/softserve/actent/repository/ImageRepository.java
@@ -0,0 +1,9 @@
+package com.softserve.actent.repository;
+
+import com.softserve.actent.model.entity.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImageRepository extends JpaRepository<Image, Long> {
+}
