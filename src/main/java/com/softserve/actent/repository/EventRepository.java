284f54ec1ae668417560eb6d5a8bed24cda00b74
RBarnabass <rastafarimood@gmail.com>
2019-03-07 14:11:41 +0200

    deleted empty space

--- src/main/java/com/softserve/actent/repository/EventRepository.java
+++ src/main/java/com/softserve/actent/repository/EventRepository.java
@@ -3,7 +3,6 @@ package com.softserve.actent.repository;
 import com.softserve.actent.model.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
