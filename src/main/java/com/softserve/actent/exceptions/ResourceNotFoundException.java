6a83696deb3501d4b545357a0824713e85192772
Senchakmen <senchakevuch@gmail.com>
2019-03-07 18:03:11 +0200

    resolve marge conflict

--- src/main/java/com/softserve/actent/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/softserve/actent/exceptions/ResourceNotFoundException.java
@@ -4,7 +4,7 @@ import com.softserve.actent.exceptions.codes.ExceptionCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-<<<<<<< HEAD
+
 /**
  * <h2>General exception for validation</h2>
  * <p>
@@ -12,9 +12,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  * </p>
  */
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-=======
-@ResponseStatus(HttpStatus.NOT_FOUND)
->>>>>>> 60d91744d8002360822b3818831ca0715d931ad0
+
 public class ResourceNotFoundException extends ActentAppException {
 
     public ResourceNotFoundException(String message, ExceptionCode exceptionCode) {
