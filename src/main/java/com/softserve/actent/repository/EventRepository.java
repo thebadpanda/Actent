5be196e9d4b97cbe7e3c760cc05cbf62da65c005
RBarnabass <rastafarimood@gmail.com>
2019-03-07 14:07:15 +0200

    changet return value to optional

--- src/main/java/com/softserve/actent/repository/EventRepository.java
+++ src/main/java/com/softserve/actent/repository/EventRepository.java
@@ -4,8 +4,10 @@ import com.softserve.actent.model.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
 
-    Event findByTitle(String title);
+    Optional<Event> findByTitle(String title);
 }
