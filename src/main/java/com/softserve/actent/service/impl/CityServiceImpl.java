f4aa348874e365c22d69ff3f4d5291ccab35c8c0
Ira Shturyn <ira.shturyn@gmail.com>
2019-03-07 23:08:50 +0200

    change in get method getOne to findById

---
+++ src/main/java/com/softserve/actent/service/impl/CityServiceImpl.java
@@ -0,0 +1,58 @@
+package com.softserve.actent.service.impl;
+
+import com.softserve.actent.model.entity.City;
+import com.softserve.actent.repository.CityRepository;
+import com.softserve.actent.service.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CityServiceImpl implements CityService {
+
+    private final CityRepository cityRepository;
+
+    @Autowired
+    public CityServiceImpl(CityRepository cityRepository) {
+        this.cityRepository = cityRepository;
+    }
+
+    @Override
+    public City add(City city) {
+        return cityRepository.save(city);
+    }
+
+    @Override
+    public List<City> getAll() {
+        return cityRepository.findAll();
+    }
+
+    @Override
+    public City get(Long id) {
+        Optional<City> optionalCity = cityRepository.findById(id);
+        return optionalCity.orElse(null);
+    }
+
+    @Override
+    public City update(City city, Long id) {
+        if (cityRepository.findById(id).isPresent()) {
+            city.setId(id);
+            return cityRepository.save(city);
+        } else {
+            // TODO: else throw exception
+            return null;
+        }
+    }
+
+    @Override
+    public void delete(Long id) {
+        cityRepository.deleteById(id);
+    }
+
+    @Override
+    public List<City> getByRegionId(Long regionId) {
+        return cityRepository.getAllByRegionId(regionId);
+    }
+}
