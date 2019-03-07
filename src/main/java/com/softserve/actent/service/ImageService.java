e8bf6b987c609429121e0f53dbf56ad22b912191
Oleh Volchkov <volchkov.oleh@gmail.com>
2019-03-07 17:43:32 +0200

    extended base crud service in image service

--- src/main/java/com/softserve/actent/service/ImageService.java
+++ src/main/java/com/softserve/actent/service/ImageService.java
@@ -4,19 +4,9 @@ import com.softserve.actent.model.entity.Image;
 
 import java.util.List;
 
-public interface ImageService {
-
-    Image addImage(Image image);
-
-    Image getImageById(Long imageId);
+public interface ImageService extends BaseCrudService<Image> {
 
     Image getImageByFilePath(String filePath);
 
     Image getImageByHash(String hash);
-
-    List<Image> getAllImages();
-
-    Image updateImageById(Image image, Long imageId);
-
-    void deleteImageById(Long imageId);
 }
