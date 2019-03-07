c224b79600e0ba570f79751585b9c2ec2109b1d1
Ira Shturyn <ira.shturyn@gmail.com>
2019-03-07 11:25:00 +0200

    add @NoArgsConstructor to city, country, regoin, location dto

--- src/main/java/com/softserve/actent/model/dto/CountryDto.java
+++ src/main/java/com/softserve/actent/model/dto/CountryDto.java
@@ -1,9 +1,11 @@
 package com.softserve.actent.model.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 @Data
+@NoArgsConstructor
 public class CountryDto {
     @NonNull
     private String name;
