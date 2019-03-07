56a81ac3e5d55fcbf550bdab41a4d7e5d05dea2d
Oleh Volchkov <volchkov.oleh@gmail.com>
2019-03-07 17:32:23 +0200

    removed commented code

--- src/main/java/com/softserve/actent/service/ReviewService.java
+++ src/main/java/com/softserve/actent/service/ReviewService.java
@@ -3,12 +3,5 @@ package com.softserve.actent.service;
 import com.softserve.actent.model.entity.Review;
 
 public interface ReviewService extends BaseCrudService<Review> {
-
-//    Review addReview(Review review);
-//
-//    Review getReviewById(Long reviewId);
-//
-//    Review updateReviewById(Review review, Long reviewId);
-//
-//    void deleteReviewById(Long reviewId);
+    
 }
