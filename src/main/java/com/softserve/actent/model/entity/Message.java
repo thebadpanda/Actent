020a69cd073d4f1af4cea3a0c5aec89766b3ee9c
octap16 <octap17@gmail.com>
2019-03-08 16:41:14 +0200

    add nullable = false and updatable = false

--- src/main/java/com/softserve/actent/model/entity/Message.java
+++ src/main/java/com/softserve/actent/model/entity/Message.java
@@ -5,14 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import org.hibernate.annotations.CreationTimestamp;
 
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 
@@ -32,8 +25,8 @@ public class Message {
 
     private String messageContent;
 
-    @NonNull
     @CreationTimestamp
+    @Column(updatable = false)
     private LocalDateTime sendTime;
 
     @ManyToOne
@@ -41,10 +34,12 @@ public class Message {
 
     @NonNull
     @ManyToOne
+    @JoinColumn(nullable = false)
     private User sender;
 
     @NonNull
     @ManyToOne
+    @JoinColumn(nullable = false)
     private Chat chat;
 
 }
