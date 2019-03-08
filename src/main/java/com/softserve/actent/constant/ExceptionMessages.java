0be817ac4b6eaba57905715836563ef64d1e7b56
octap16 <octap17@gmail.com>
2019-03-08 16:37:45 +0200

    create error message for messages

--- src/main/java/com/softserve/actent/constant/ExceptionMessages.java
+++ src/main/java/com/softserve/actent/constant/ExceptionMessages.java
@@ -7,4 +7,7 @@ public class ExceptionMessages {
     public static final String EQUIPMENT_BY_THIS_ID_IS_NOT_FOUND = "Equipment by this id is not found";
     public static final String EQUIPMENTS_ARE_NOT_FOUND = "Equipments are not found";
     public static final String USER_BY_THIS_ID_IS_NOT_FOUND = "User by this id is not found";
+
+    public static final String YOU_CAN_NOT_CHANGE_THIS_MESSAGE = "You can't change this message";
+    public static final String MESSAGE_NOT_FOUND = "Message not found";
 }
