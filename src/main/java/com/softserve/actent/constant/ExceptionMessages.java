08f0575340283dec89a825f4066940c7af9fc4ca
Senchakmen <senchakevuch@gmail.com>
2019-03-08 15:35:42 +0200

    adds string constants

--- src/main/java/com/softserve/actent/constant/ExceptionMessages.java
+++ src/main/java/com/softserve/actent/constant/ExceptionMessages.java
@@ -4,4 +4,7 @@ public class ExceptionMessages {
 
     public static final String EVENT_BY_THIS_ID_IS_NOT_FOUND = "Event by this id is not found";
     public static final String EVENT_BY_THIS_TITLE_IS_NOT_FOUND = "Event by this title is not found";
+    public static final String EQUIPMENT_BY_THIS_ID_IS_NOT_FOUND = "Equipment by this id is not found";
+    public static final String EQUIPMENTS_ARE_NOT_FOUND = "Equipments are not found";
+    public static final String USER_BY_THIS_ID_IS_NOT_FOUND = "User by this id is not found";
 }
