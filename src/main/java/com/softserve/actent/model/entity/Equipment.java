d763d4e0e29e43e753a9ae1df10bc83ead20896d
Senchakmen <senchakevuch@gmail.com>
2019-03-08 16:06:43 +0200

    change entity field assignedEvent

--- src/main/java/com/softserve/actent/model/entity/Equipment.java
+++ src/main/java/com/softserve/actent/model/entity/Equipment.java
@@ -5,13 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
 @Data
@@ -42,6 +36,7 @@ public class Equipment {
     private User assignedUser;
 
     @NonNull
+    @JoinColumn(nullable = false)
     @ManyToOne
     private Event assignedEvent;
 
