18108a0262d2cb8c2e2cceab3e164207c2f71538
Ira Shturyn <ira.shturyn@gmail.com>
2019-03-08 10:40:30 +0200

    delete LocationConverter, add modelMapper to controller

--- src/main/java/com/softserve/actent/controller/LocationController.java
+++ src/main/java/com/softserve/actent/controller/LocationController.java
@@ -2,7 +2,6 @@ package com.softserve.actent.controller;
 
 import com.softserve.actent.model.dto.IdDto;
 import com.softserve.actent.model.dto.LocationDto;
-import com.softserve.actent.model.dto.converter.LocationConverter;
 import com.softserve.actent.model.entity.Location;
 import com.softserve.actent.service.CityService;
 import com.softserve.actent.service.LocationService;
@@ -10,9 +9,18 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -20,16 +28,13 @@ public class LocationController {
 
     private final LocationService locationService;
 
-    private final LocationConverter locationConverter;
-
     private final CityService cityService;
 
     private final ModelMapper modelMapper;
 
     @Autowired
-    public LocationController(LocationService locationService, LocationConverter locationConverter, CityService cityService, ModelMapper modelMapper) {
+    public LocationController(LocationService locationService, CityService cityService, ModelMapper modelMapper) {
         this.locationService = locationService;
-        this.locationConverter = locationConverter;
         this.cityService = cityService;
         this.modelMapper = modelMapper;
     }
@@ -43,12 +48,14 @@ public class LocationController {
         if (location == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(locationConverter.convertToDto(location), HttpStatus.OK);
+        LocationDto locationDto = modelMapper.map(location, LocationDto.class);
+        return new ResponseEntity<>(locationDto, HttpStatus.OK);
     }
 
     @GetMapping(value = "/locations")
     public ResponseEntity<List<LocationDto>> getAll(@RequestParam(value = "cityId", required = false) Long cityId) {
         List<Location> locations;
+        List<LocationDto> locationDtos;
         if (cityId == null) {
             locations = locationService.getAll();
         } else {
@@ -57,16 +64,23 @@ public class LocationController {
         if (locations.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(locationConverter.convertToDto(locations), HttpStatus.OK);
+        locationDtos = locations.stream()
+                .map(location -> modelMapper.map(location, LocationDto.class))
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(locationDtos, HttpStatus.OK);
     }
 
     @PostMapping(value = "/locations")
-    public ResponseEntity<IdDto> add(@RequestBody LocationDto locationDto) {
-        Location location = new Location();
-        location.setAddress(locationDto.getAddress());
-        location.setCity(cityService.get(locationDto.getCityId()));
-        locationService.add(location);
-        return new ResponseEntity<>(new IdDto(location.getId()), HttpStatus.CREATED);
+    public ResponseEntity<LocationDto> add(@RequestBody LocationDto locationDto) {
+        Location newLocation = modelMapper.map(locationDto, Location.class);
+        newLocation.setAddress(locationDto.getAddress());
+        newLocation.setCity(cityService.get(locationDto.getCityId()));
+
+        Location location = locationService.add(newLocation);
+        locationDto = modelMapper.map(location, LocationDto.class);
+        locationDto.setAddress(location.getAddress());
+        locationDto.setCityId(location.getCity().getId());
+        return new ResponseEntity<>(locationDto, HttpStatus.CREATED);
     }
 
     @PutMapping(value = "/locations/{id}")
