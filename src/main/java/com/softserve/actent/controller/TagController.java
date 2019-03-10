5568932bee335028ee823b224389c10019296a6f
Oleh Volchkov <volchkov.oleh@gmail.com>
2019-03-11 00:05:06 +0200

    implemented input data validation on controller level

--- src/main/java/com/softserve/actent/controller/TagController.java
+++ src/main/java/com/softserve/actent/controller/TagController.java
@@ -1,5 +1,8 @@
 package com.softserve.actent.controller;
 
+import com.softserve.actent.constant.ExceptionMessages;
+import com.softserve.actent.exceptions.codes.ExceptionCode;
+import com.softserve.actent.exceptions.validation.IncorrectStringException;
 import com.softserve.actent.model.dto.IdDto;
 import com.softserve.actent.model.dto.TagDto;
 import com.softserve.actent.model.entity.Tag;
@@ -18,7 +21,6 @@ import java.util.stream.Collectors;
 public class TagController {
 
     private final TagService tagService;
-
     private final ModelMapper modelMapper;
 
     @Autowired
@@ -31,10 +33,15 @@ public class TagController {
     @ResponseStatus(HttpStatus.CREATED)
     public IdDto addTag(@RequestBody TagDto tagDto) {
 
-        Tag tag = modelMapper.map(tagDto, Tag.class);
-        tag = tagService.add(tag);
+        if (tagDto.getText() == null || tagDto.getText().length() < 3) {
+            throw new IncorrectStringException(ExceptionMessages.TOO_SHORT_TAG_TEXT,
+                    ExceptionCode.INCORRECT_STRING);
+        } else {
+            Tag tag = modelMapper.map(tagDto, Tag.class);
+            tag = tagService.add(tag);
 
-        return new IdDto(tag.getId());
+            return new IdDto(tag.getId());
+        }
     }
 
     @GetMapping(value = "/tags/{id}")
@@ -50,19 +57,23 @@ public class TagController {
     public List<TagDto> getTags() {
 
         List<Tag> tags = tagService.getAll();
-        List<TagDto> imagesDto = tags.stream()
+
+        return tags.stream()
                 .map(image -> modelMapper.map(image, TagDto.class))
                 .collect(Collectors.toList());
-
-        return imagesDto;
     }
 
     @PutMapping(value = "/tags/{id}")
     @ResponseStatus(HttpStatus.OK)
     public TagDto updateTagById(@RequestBody TagDto tagDto, @PathVariable Long id) {
 
-        Tag tag = tagService.update(modelMapper.map(tagDto, Tag.class), id);
-        return modelMapper.map(tag, TagDto.class);
+        if (tagDto.getText() == null || tagDto.getText().length() < 3) {
+            throw new IncorrectStringException(ExceptionMessages.TOO_SHORT_TAG_TEXT,
+                    ExceptionCode.INCORRECT_STRING);
+        } else {
+            Tag tag = tagService.update(modelMapper.map(tagDto, Tag.class), id);
+            return modelMapper.map(tag, TagDto.class);
+        }
     }
 
     @DeleteMapping(value = "/tags/{id}")
