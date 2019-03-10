05f257f3c779906b12e76434ff0d0d38b2546f83
Nazar <nazarmavko@gmail.com>
2019-03-10 22:15:07 +0200

    added flow for Category

--- src/main/java/com/softserve/actent/service/CategoryService.java
+++ src/main/java/com/softserve/actent/service/CategoryService.java
@@ -2,5 +2,10 @@ package com.softserve.actent.service;
 
 import com.softserve.actent.model.entity.Category;
 
-public interface CategoryService extends BaseCrudService<Category>{
-    }
+import java.util.List;
+
+public interface CategoryService extends BaseCrudService<Category> {
+    List<Category> getSubcategories(Category parent);
+    Category getParentByName(String name);
+}
+
