774bf134d01f3c11b32a2aedb8b8c118752282c7
onlyVone <43787744+onlyVone@users.noreply.github.com>
2019-03-10 22:48:29 +0200

    Refactor class Chat.java -> Add throwing exceptions(service) and rewrite controller

--- src/main/java/com/softserve/actent/constant/ExceptionMessages.java
+++ src/main/java/com/softserve/actent/constant/ExceptionMessages.java
@@ -7,4 +7,6 @@ public class ExceptionMessages {
     public static final String EQUIPMENT_BY_THIS_ID_IS_NOT_FOUND = "Equipment by this id is not found";
     public static final String EQUIPMENTS_ARE_NOT_FOUND = "Equipments are not found";
     public static final String USER_BY_THIS_ID_IS_NOT_FOUND = "User by this id is not found";
+    public static final String CHAT_BY_THIS_ID_IS_NOT_FOUND = "Chat by this id is not found";
+    public static final String ACTIVE_BY_THIS_TYPE_IS_NOT_FOUND = "While adding chat to active. Active by this type is not found";
 }
