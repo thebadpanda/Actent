afc64c3cc9e8b006d2ec3999a0116c7b19fa0caa
octap16 <octap17@gmail.com>
2019-03-11 20:03:23 +0200

    change anotation to @Positive

--- src/main/java/com/softserve/actent/model/dto/message/CreateImageMessageDto.java
+++ src/main/java/com/softserve/actent/model/dto/message/CreateImageMessageDto.java
@@ -4,18 +4,18 @@ import com.softserve.actent.constant.StringConstants;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
 @Data
 public class CreateImageMessageDto {
 
-    @Min(value = 1, message = StringConstants.SENDER_ID_MUST_BE_GREATER_THAN_ZERO)
+    @Positive(message = StringConstants.SENDER_ID_MUST_BE_GREATER_THAN_ZERO)
     @NotNull(message = StringConstants.SENDER_SHOULD_NOT_BE_NULL)
     private Long senderId;
 
-    @Min(value = 1, message = StringConstants.CHAT_ID_MUST_BE_GREATER_THAN_ZERO)
+    @Positive(message = StringConstants.CHAT_ID_MUST_BE_GREATER_THAN_ZERO)
     @NotNull(message = StringConstants.CHAT_ID_SHOULD_NOT_BE_NULL)
     private Long chatId;
 
