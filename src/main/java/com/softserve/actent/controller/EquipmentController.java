c7eb12ba3f7acd8ddf901251d603c0c533dd808a
Senchakmen <senchakevuch@gmail.com>
2019-03-11 14:54:57 +0200

    code refuctor for EquipmentController

--- src/main/java/com/softserve/actent/controller/EquipmentController.java
+++ src/main/java/com/softserve/actent/controller/EquipmentController.java
@@ -4,10 +4,7 @@ import com.softserve.actent.model.dto.IdDto;
 import com.softserve.actent.model.dto.equipment.EquipmentCreateDto;
 import com.softserve.actent.model.dto.equipment.EquipmentDto;
 import com.softserve.actent.model.entity.Equipment;
-import com.softserve.actent.repository.UserRepository;
 import com.softserve.actent.service.impl.EquipmentServiceImpl;
-import com.softserve.actent.service.impl.EventServiceImpl;
-import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Log4j2
 @RestController
 @RequestMapping("/api/v1")
 public class EquipmentController {
@@ -33,7 +29,7 @@ public class EquipmentController {
 
     @GetMapping("/equipments")
     @ResponseStatus(HttpStatus.OK)
-    public List<EquipmentDto> getAllEquipments(){
+    public List<EquipmentDto> getAllEquipments() {
 
         List<Equipment> equipments = equipmentServiceImpl.getAll();
 
@@ -44,7 +40,7 @@ public class EquipmentController {
 
     @GetMapping("/equipments/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public EquipmentDto getEquipmentById(@PathVariable Long id){
+    public EquipmentDto getEquipmentById(@PathVariable Long id) {
 
         Equipment equipment = equipmentServiceImpl.get(id);
 
@@ -53,7 +49,7 @@ public class EquipmentController {
 
     @PostMapping("/equipments")
     @ResponseStatus(HttpStatus.CREATED)
-    public IdDto addEquipment(@RequestBody EquipmentCreateDto equipmentCreateDto){
+    public IdDto addEquipment(@RequestBody EquipmentCreateDto equipmentCreateDto) {
 
         Equipment equipment = equipmentServiceImpl.add(modelMapper.map(equipmentCreateDto, Equipment.class));
         return new IdDto(equipment.getId());
@@ -61,14 +57,14 @@ public class EquipmentController {
 
     @DeleteMapping("/equipments/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteEquipmentById(@PathVariable Long id){
+    public void deleteEquipmentById(@PathVariable Long id) {
 
         equipmentServiceImpl.delete(id);
     }
 
     @PutMapping("/equipments/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public EquipmentCreateDto updateEquipmentById(@PathVariable Long id, @RequestBody EquipmentCreateDto equipmentCreateDto){
+    public EquipmentCreateDto updateEquipmentById(@PathVariable Long id, @RequestBody EquipmentCreateDto equipmentCreateDto) {
 
         Equipment equipment = equipmentServiceImpl.update(modelMapper.map(equipmentCreateDto, Equipment.class), id);
         return modelMapper.map(equipment, EquipmentCreateDto.class);
