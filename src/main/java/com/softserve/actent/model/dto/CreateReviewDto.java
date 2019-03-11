4763888934cb274046ae4b6fa5cc3319cd6efa4d
Oleh Volchkov <volchkov.oleh@gmail.com>
2019-03-11 21:08:59 +0200

    validation annotations in dtos

--- src/main/java/com/softserve/actent/model/dto/CreateReviewDto.java
+++ src/main/java/com/softserve/actent/model/dto/CreateReviewDto.java
@@ -1,16 +1,23 @@
 package com.softserve.actent.model.dto;
 
+import com.softserve.actent.constant.ExceptionMessages;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 @NoArgsConstructor
 public class CreateReviewDto {
 
-    @NonNull
+    @NotBlank(message = ExceptionMessages.NO_REVIEW_TEXT)
     private String text;
 
-    @NonNull
+    @NotNull(message = ExceptionMessages.NO_REVIEW_SCORE)
+    // TODO: use constants
+    @Min(value = 1, message = ExceptionMessages.BAD_REVIEW_SCORE)
+    @Max(value = 5, message = ExceptionMessages.BAD_REVIEW_SCORE)
     private Integer score;
 }
