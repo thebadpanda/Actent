a2260ade8e8a0168d6ac2609fe6dbe3e61540327
Senchakmen <senchakevuch@gmail.com>
2019-03-11 17:42:12 +0200

    add new NumberConstants

--- src/main/java/com/softserve/actent/constant/NumberConstants.java
+++ src/main/java/com/softserve/actent/constant/NumberConstants.java
@@ -3,7 +3,11 @@ package com.softserve.actent.constant;
 public class NumberConstants {
 
     public static final short TITLE_MAX_LENGTH = 100;
+    public static final short DESCRIPTION_MAX_LENGTH = 500;
     public static final short MIN_VALUE_FOR_START_DATE = 0;
     public static final short MIN_VALUE_FOR_DESCRIPTION = 0;
     public static final short MAX_VALUE_FOR_DESCRIPTION = 350;
+
+    public static final short DESCRIPTION_MIN_LENGTH = 6;
+    public static final short TITLE_MIN_LENGTH = 6;
 }
