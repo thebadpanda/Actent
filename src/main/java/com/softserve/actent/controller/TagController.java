b54f58021fcbaa2a959b07845f8f93b157ac062d
Oleh Volchkov <volchkov.oleh@gmail.com>
2019-03-12 15:59:16 +0200

    validation for pathparams and pathvariables

--- src/main/java/com/softserve/actent/controller/TagController.java
+++ src/main/java/com/softserve/actent/controller/TagController.java
@@ -1,24 +1,31 @@
 package com.softserve.actent.controller;
 
 import com.softserve.actent.constant.ExceptionMessages;
-import com.softserve.actent.exceptions.codes.ExceptionCode;
-import com.softserve.actent.exceptions.validation.IncorrectStringException;
-import com.softserve.actent.exceptions.validation.ValidationException;
 import com.softserve.actent.model.dto.CreateTagDto;
 import com.softserve.actent.model.dto.IdDto;
 import com.softserve.actent.model.dto.TagDto;
 import com.softserve.actent.model.entity.Tag;
 import com.softserve.actent.service.TagService;
-import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Validated
 @RestController
 @RequestMapping("/api/v1")
 public class TagController {
@@ -44,7 +51,8 @@ public class TagController {
 
     @GetMapping(value = "/tags/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public TagDto getTagById(@PathVariable Long id) {
+    public TagDto getTagById(@PathVariable @NotNull(message = ExceptionMessages.TAG_NO_ID)
+                                 @Positive(message = ExceptionMessages.TAG_INNAPPROPRIATE_ID) Long id) {
 
         Tag tag = tagService.get(id);
         return modelMapper.map(tag, TagDto.class);
@@ -63,7 +71,9 @@ public class TagController {
 
     @PutMapping(value = "/tags/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public TagDto updateTagById(@Validated @RequestBody CreateTagDto tagDto, @PathVariable Long id) {
+    public TagDto updateTagById(@Validated @RequestBody CreateTagDto tagDto,
+                                @PathVariable @NotNull(message = ExceptionMessages.TAG_NO_ID)
+                                @Positive(message = ExceptionMessages.TAG_INNAPPROPRIATE_ID) Long id) {
 
         Tag tag = tagService.update(modelMapper.map(tagDto, Tag.class), id);
         return modelMapper.map(tag, TagDto.class);
@@ -71,7 +81,8 @@ public class TagController {
 
     @DeleteMapping(value = "/tags/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteTagById(@PathVariable Long id) {
+    public void deleteTagById(@PathVariable @NotNull(message = ExceptionMessages.TAG_NO_ID)
+                                  @Positive(message = ExceptionMessages.TAG_INNAPPROPRIATE_ID) Long id) {
 
         tagService.delete(id);
     }
