11883a16fd53dde54081591e97f24a1562671b67
Senchakmen <senchakevuch@gmail.com>
2019-03-12 14:15:47 +0200

    replace uselles methods

--- src/main/java/com/softserve/actent/service/EquipmentService.java
+++ src/main/java/com/softserve/actent/service/EquipmentService.java
@@ -4,7 +4,4 @@ import com.softserve.actent.model.entity.Equipment;
 
 public interface EquipmentService extends BaseCrudService<Equipment>{
 
-    Boolean setSatisfied(Long equipmentId);
-    Boolean setUnsatisfied(Long equipmentId);
-    Equipment assignUser(Long userId, Long equipmentId);
 }
