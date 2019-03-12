618a9589cb301644a05bcf7bf4a6b5c54e6590ce
Arsen <arsenemarkiv@gmail.com>
2019-03-12 20:12:44 +0200

    add to string constants

--- src/main/java/com/softserve/actent/constant/StringConstants.java
+++ src/main/java/com/softserve/actent/constant/StringConstants.java
@@ -26,8 +26,10 @@ public class StringConstants {
     public static final String SEX_TOO_LONG = "Sex must be not longer than 20 symbols";
     public static final String EMPTY_USER_ID = "User id should not be null";
     public static final String USER_ID_SHOULD_BE_GREATER_THAN_ZERO = "User id should be greater than zero";
-    public static final String USER_LOCATION_LENGHT_RANGE = "Location should not be more that 50 symbols";
+    public static final String USER_LOCATION_LENGHT_RANGE = "Location should not be more than 50 symbols";
     public static final String USER_CITY_ID_NEGATIVE_NUMBER = "User id should not be negative number";
+    public static final String USER_ROLE_TOO_LONG = "User role should not be more than 20 symbols";
+    public static final String USER_AVATAR_ID_SHOULD_BE_GREATER_THAN_ZERO = "User avatar id should be greater than zero";
 
     public static final String EMPTY_COUNTRY ="Country can't be empty ";
     public static final String EMPTY_REGION ="Region can't be empty";
---
+++ src/main/java/com/softserve/actent/model/entity/Sex.java
@@ -0,0 +1,4 @@
+package com.softserve.actent.model.entity;
+
+public class Sex {
+}
