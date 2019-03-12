5b86889ee59918fa67b66037cabd720e3a3f26a0
Senchakmen <senchakevuch@gmail.com>
2019-03-12 13:46:57 +0200

    rename parameters for methods

--- src/main/java/com/softserve/actent/service/EquipmentService.java
+++ src/main/java/com/softserve/actent/service/EquipmentService.java
@@ -1,11 +1,10 @@
 package com.softserve.actent.service;
 
 import com.softserve.actent.model.entity.Equipment;
-import com.softserve.actent.model.entity.User;
 
 public interface EquipmentService extends BaseCrudService<Equipment>{
 
-    Boolean setSatisfied(Long id);
-    Boolean setUnsatisfied(Long id);
-    Equipment assignUser(User user, Long id);
+    Boolean setSatisfied(Long equipmentId);
+    Boolean setUnsatisfied(Long equipmentId);
+    Equipment assignUser(Long userId, Long equipmentId);
 }
