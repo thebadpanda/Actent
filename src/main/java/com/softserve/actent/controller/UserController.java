2de22641de433245db3eb930083a4e5b1be4fc30
Arsen <arsenemarkiv@gmail.com>
2019-03-12 13:02:43 +0200

    UserController

--- src/main/java/com/softserve/actent/controller/UserController.java
+++ src/main/java/com/softserve/actent/controller/UserController.java
@@ -11,7 +11,6 @@ import com.softserve.actent.service.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
