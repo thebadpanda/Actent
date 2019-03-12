831cbdeed07bb2cd6cb7b5e33ece887de60855a6
RBarnabass <rastafarimood@gmail.com>
2019-03-12 17:59:57 +0200

    Added city dto for event

---
+++ src/main/java/com/softserve/actent/model/dto/event/CityForEventDto.java
@@ -0,0 +1,12 @@
+package com.softserve.actent.model.dto.event;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class CityForEventDto {
+
+    private Long id;
+    private String name;
+}
