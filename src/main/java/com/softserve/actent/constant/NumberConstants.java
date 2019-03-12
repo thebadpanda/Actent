d48252904e3e1a03248f61d2e97f36446c759dc8
Ira Shturyn <ira.shturyn@gmail.com>
2019-03-12 13:00:42 +0200

    add constants for country,region,city and location

--- src/main/java/com/softserve/actent/constant/NumberConstants.java
+++ src/main/java/com/softserve/actent/constant/NumberConstants.java
@@ -11,4 +11,16 @@ public class NumberConstants {
 
     public static final short DESCRIPTION_MIN_LENGTH = 6;
     public static final short TITLE_MIN_LENGTH = 6;
+
+    public static final short COUNTRY_MIN_LENGTH = 3;
+    public static final short COUNTRY_MAX_LENGTH = 30;
+
+    public static final short REGION_MIN_LENGTH = 5;
+    public static final short REGION_MAX_LENGTH = 30;
+
+    public static final short CITY_MIN_LENGTH = 4;
+    public static final short CITY_MAX_LENGTH = 25;
+
+    public static final short LOCATION_MIN_LENGTH = 5;
+    public static final short LOCATION_MAX_LENGTH = 50;
 }
