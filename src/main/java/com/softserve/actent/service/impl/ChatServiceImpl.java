dd76fc98a88443a5a106299ff7a36bc37042a20b
onlyVone <43787744+onlyVone@users.noreply.github.com>
2019-03-12 21:55:28 +0200

    Refactor chat logic

--- src/main/java/com/softserve/actent/service/impl/ChatServiceImpl.java
+++ src/main/java/com/softserve/actent/service/impl/ChatServiceImpl.java
@@ -14,9 +14,7 @@ import com.softserve.actent.service.ChatService;
 import com.softserve.actent.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import javax.transaction.Transactional;
-import java.util.List;
 
 @Service
 public class ChatServiceImpl implements ChatService {
