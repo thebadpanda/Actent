a173912cb8f1a81bd1ea905a4952936e1e983e15
Oleh Volchkov <volchkov.oleh@gmail.com>
2019-03-12 23:31:42 +0200

    added test for tag service

---
+++ src/test/java/com/softserve/actent/service/impl/TagServiceImplTest.java
@@ -0,0 +1,128 @@
+package com.softserve.actent.service.impl;
+
+import com.softserve.actent.exceptions.ResourceNotFoundException;
+import com.softserve.actent.model.entity.Tag;
+import com.softserve.actent.repository.TagRepository;
+import com.softserve.actent.service.TagService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TagServiceImplTest {
+
+    @Autowired
+    private TagService tagService;
+
+    @MockBean
+    private TagRepository tagRepository;
+
+    private final Long firstTagId = 1L;
+    private final Long secondTagId = 2L;
+    private final Long nonExistingTagId = 99L;
+    private final String firstTagText = "beer";
+    private final String secondTagText = "football";
+    private final int tagsCount = 2;
+
+    @Before
+    public void setUp() {
+
+        Tag firstTag = new Tag();
+        firstTag.setId(firstTagId);
+        firstTag.setText(firstTagText);
+
+        Tag secondTag = new Tag();
+        secondTag.setId(secondTagId);
+        secondTag.setText(secondTagText);
+
+        List<Tag> tags = Arrays.asList(firstTag, secondTag);
+
+        Mockito.when(tagRepository.findById(firstTagId)).thenReturn(Optional.of(firstTag));
+        Mockito.when(tagRepository.findById(secondTagId)).thenReturn(Optional.of(secondTag));
+        Mockito.when(tagRepository.findById(nonExistingTagId)).thenReturn(Optional.empty());
+        Mockito.when(tagRepository.findAll()).thenReturn(tags);
+        Mockito.when(tagRepository.save(firstTag)).thenReturn(firstTag);
+        Mockito.when(tagRepository.save(secondTag)).thenReturn(secondTag);
+        Mockito.doNothing().when(tagRepository).deleteById(firstTagId);
+        Mockito.doNothing().when(tagRepository).deleteById(secondTagId);
+    }
+
+    @Test
+    public void whenExistingId_thenTagShouldBeFound() {
+
+        assertThat(tagService.get(firstTagId).getText()).isEqualTo(firstTagText);
+    }
+
+    @Test(expected = ResourceNotFoundException.class)
+    public void whenNonExistingId_thenExceptionShouldBeThrown() {
+
+        tagService.get(nonExistingTagId);
+    }
+
+    @Test
+    public void whenGetAll_thenListOfTagsShouldBeReturned() {
+
+        assertThat(tagService.getAll().size()).isEqualTo(tagsCount);
+        assertThat(tagService.getAll().get(0).getText()).isEqualTo(firstTagText);
+        assertThat(tagService.getAll().get(1).getText()).isEqualTo(secondTagText);
+    }
+
+    @Test
+    public void whenAddTag_thenTagShouldBeReturned() {
+
+        Tag tag = new Tag();
+        tag.setId(firstTagId);
+        tag.setText(firstTagText);
+
+        assertThat(tagService.add(tag)).isEqualTo(tag);
+    }
+
+    @Test
+    public void whenUpdateTagWithExistingId_thenTagShouldBeReturned() {
+
+        Tag tag = new Tag();
+        tag.setId(secondTagId);
+        tag.setText(secondTagText);
+
+        assertThat(tagService.update(tag, secondTagId).getText()).isEqualTo(secondTagText);
+    }
+
+    @Test(expected = ResourceNotFoundException.class)
+    public void whenUpdateTagWithNonExistingId_thenExceptionShouldBeThrown() {
+
+        Tag tag = new Tag();
+        tag.setId(secondTagId);
+        tag.setText(secondTagText);
+
+        tagService.update(tag, nonExistingTagId);
+    }
+
+    @Test
+    public void whenDeleteTagWithExistingId_thenNothingShouldBeReturned() {
+
+        tagService.delete(firstTagId);
+    }
+    
+    @Test(expected = ResourceNotFoundException.class)
+    public void whenDeleteTagWithNonExistingId_thenExceptionShouldBeThrown() {
+
+        tagService.delete(nonExistingTagId);
+    }
+
+    @After
+    public void tearDown() {
+    }
+}
