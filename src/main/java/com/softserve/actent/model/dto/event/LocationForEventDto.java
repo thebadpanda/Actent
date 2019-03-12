78cc09069904cd7a9e81578246dd732169db7466
RBarnabass <rastafarimood@gmail.com>
2019-03-12 18:13:04 +0200

    Added location for event dto

--- src/main/java/com/softserve/actent/model/dto/event/LocationForEventDto.java
+++ src/main/java/com/softserve/actent/model/dto/event/LocationForEventDto.java
@@ -1,14 +1,16 @@
 package com.softserve.actent.model.dto.event;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 
 @Data
 @NoArgsConstructor
-class LocationForEventDto {
+public class LocationForEventDto {
 
-    @NonNull
-    private Long cityId;
+    private Long id;
     private String address;
+
+    @JsonProperty("Country")
+    private CountryForEventDto countryForEventDto;
 }
