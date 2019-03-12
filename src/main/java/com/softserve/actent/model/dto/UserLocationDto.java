b5dd81aa662b8c3d6158c483a710a38f8819a1be
Arsen <arsenemarkiv@gmail.com>
2019-03-12 14:42:47 +0200

    dto constants for validation

--- src/main/java/com/softserve/actent/model/dto/UserLocationDto.java
+++ src/main/java/com/softserve/actent/model/dto/UserLocationDto.java
@@ -1,5 +1,6 @@
 package com.softserve.actent.model.dto;
 
+import com.softserve.actent.constant.NumberConstants;
 import com.softserve.actent.constant.StringConstants;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
@@ -9,7 +10,7 @@ import javax.validation.constraints.Positive;
 @Data
 public class UserLocationDto {
 
-    @Length(max = 50, message = StringConstants.USER_LOCATION_LENGHT_RANGE)
+    @Length(max = NumberConstants.USER_ADDRESS_MAX_LENGTH, message = StringConstants.USER_LOCATION_LENGHT_RANGE)
     private String address;
 
     @Positive(message = StringConstants.USER_CITY_ID_NEGATIVE_NUMBER)
--- src/main/java/com/softserve/actent/model/dto/UserSettingsDto.java
+++ src/main/java/com/softserve/actent/model/dto/UserSettingsDto.java
@@ -1,48 +1,42 @@
 package com.softserve.actent.model.dto;
 
-<<<<<<< HEAD
+import com.softserve.actent.constant.NumberConstants;
 import com.softserve.actent.constant.StringConstants;
-=======
->>>>>>> user_chain
 import com.softserve.actent.model.entity.Image;
 import com.softserve.actent.model.entity.Location;
 import com.softserve.actent.model.entity.Role;
 import lombok.Data;
-<<<<<<< HEAD
 import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Future;
 import javax.validation.constraints.NotBlank;
-=======
 
->>>>>>> user_chain
 import java.time.LocalDate;
 
 @Data
 public class UserSettingsDto {
 
-<<<<<<< HEAD
     @NotBlank(message = StringConstants.EMPTY_USER_LOGIN)
-    @Length(max = 20, min = 5, message = StringConstants.USER_LOGIN_LENGTH_RANGE)
+    @Length(max = NumberConstants.USER_LOGIN_MAX_LENGTH, min = NumberConstants.USER_LOGIN_MIN_LENGTH, message = StringConstants.USER_LOGIN_LENGTH_RANGE)
     private String login;
 
     @NotBlank(message = StringConstants.EMPTY_USER_EMAIL)
-    @Length(max = 30, message = StringConstants.USER_EMAIL_LENGTH_RANGE)
+    @Length(max = NumberConstants.USER_EMAIL_MAX_LENGTH, message = StringConstants.USER_EMAIL_LENGTH_RANGE)
     @Email(message = StringConstants.EMAIL_NOT_VALID)
     private String email;
 
     @NotBlank(message = StringConstants.EMPTY_USER_FIRST_NAME)
-    @Length(max = 20, min = 2, message = StringConstants.USER_FIRST_NAME_LENGTH_RANGE)
+    @Length(max = NumberConstants.USER_FIRST_NAME_MAX_LENGTH, min = NumberConstants.USER_FIRST_NAME_MIN_LENGTH, message = StringConstants.USER_FIRST_NAME_LENGTH_RANGE)
     private String firstName;
 
     @NotBlank(message = StringConstants.EMPTY_USER_LAST_NAME)
-    @Length(max = 20, min = 2, message = StringConstants.USER_LAST_NAME_LENGTH_RANGE)
+    @Length(max = NumberConstants.USER_LAST_NAME_MAX_LENGTH, min = NumberConstants.USER_LAST_NAME_MIN_LENGTH, message = StringConstants.USER_LAST_NAME_LENGTH_RANGE)
     private String lastName;
 
     @NotBlank(message = StringConstants.EMPTY_USER_PASSWORD)
-    @Length(max = 30, min = 6, message = StringConstants.USER_PASSWORD_LENGTH_RANGE)
+    @Length(max = NumberConstants.USER_PASSWORD_MAX_LENGTH, min = NumberConstants.USER_PASSWORD_MIN_LENGTH, message = StringConstants.USER_PASSWORD_LENGTH_RANGE)
     private String password;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
@@ -51,26 +45,13 @@ public class UserSettingsDto {
 
     private Image avatar;
 
-    private UserLocationDto location;
-
-    @Length(max = 500, message = StringConstants.BIO_TOO_LONG)
-    private String bio;
-
-    @Length(max = 20, message = StringConstants.SEX_TOO_LONG)
-    private String sex;
-
-=======
-    private String login;
-    private String email;
-    private String firstName;
-    private String lastName;
-    private String password;
-    private LocalDate birthDate;
-    private Image avatar;
     private Location location;
-    private String bio;
-    private String sex;
->>>>>>> user_chain
-    private Role role;
 
+    @Length(max = NumberConstants.USER_BIO_MAX_LENGTH, message = StringConstants.BIO_TOO_LONG)
+    private String bio;
+
+    @Length(max = NumberConstants.USER_SEX_MAX_LENGTH, message = StringConstants.SEX_TOO_LONG)
+    private String sex;
+
+    private Role role;
 }
