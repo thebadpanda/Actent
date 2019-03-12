d4585c2e616013d67c4c617f204f1bd08e9ee416
Senchakmen <senchakevuch@gmail.com>
2019-03-12 23:30:49 +0200

    add test for get and getAll methods

--- src/test/java/com/softserve/actent/service/impl/EquipmentServiceImplTest.java
+++ src/test/java/com/softserve/actent/service/impl/EquipmentServiceImplTest.java
@@ -1,6 +1,6 @@
-/*
 package com.softserve.actent.service.impl;
 
+import com.softserve.actent.exceptions.ResourceNotFoundException;
 import com.softserve.actent.model.entity.Equipment;
 import com.softserve.actent.model.entity.Event;
 import com.softserve.actent.model.entity.User;
@@ -12,85 +12,98 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.mockito.internal.verification.VerificationModeFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
-
-*/
-/**
-
-*//*
-
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EquipmentServiceImplTest {
 
-    @MockBean
-    private static EquipmentRepository equipmentRepository;
-
-    @MockBean
-    private static UserRepository userRepository;
-
-    @MockBean
-    private static EventRepository eventRepository;
-
-    @MockBean
-    private static UserServiceImpl userServiceImpl;
-
-//    @TestConfiguration
-//    static class EquipmentServiceImplTestContextConfiguration {
-//        @Bean
-//        public EquipmentService equipmentService() {
-//            return new EquipmentServiceImpl(equipmentRepository, eventRepository, userRepository, userServiceImpl);
-//        }
-//    }
-
     @Autowired
     private EquipmentService equipmentService;
+
+    @MockBean
+    private EquipmentRepository equipmentRepository;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private EventRepository eventRepository;
+
+    private final Long existingId = 1L;
+    private final Long secondExistingId = 2L;
+    private final Long nonExistingId = -1L;
+    private final String title = "title";
+    private final String secondTitle = "secondTitle";
+    private final String description = "description";
+    private final Boolean satisfied = false;
+    private final Integer equipmentCount = 2;
+
 
     @Before
     public void setUp() {
 
         Event event = new Event();
         User user = new User();
-        Equipment equipment = new Equipment(1L, "Test", "description", true, user, event);
 
-        Mockito.when(equipmentRepository.findById(1L))
+        Equipment equipment = new Equipment(existingId, title, description, satisfied, user, event);
+        Equipment secondEquipment = new Equipment(secondExistingId, secondTitle, description, satisfied, user, event);
+
+        List<Equipment> allEquipments = Arrays.asList(equipment, secondEquipment);
+
+        Mockito.when(equipmentRepository.findById(existingId))
                 .thenReturn(Optional.of(equipment));
-
+        Mockito.when(equipmentRepository.findById(nonExistingId))
+                .thenReturn(Optional.empty());
+        Mockito.when(equipmentRepository.findAll())
+                .thenReturn(allEquipments);
     }
 
     @Test
-    public void add() {
+    public void whenValidId_thenEquipmentShouldBeFound() {
+
+        Equipment found = equipmentService.get(existingId);
+        verifyFindByIdIsCalledOnce();
+        assertThat(found.getTitle()).isEqualTo(title);
+    }
+
+    @Test(expected = ResourceNotFoundException.class)
+    public void whenInValidId_thenEmployeeShouldNotBeFound(){
+
+        Equipment found = equipmentService.get(nonExistingId);
+        verifyFindByIdIsCalledOnce();
+        assertThat(found).isNull();
     }
 
     @Test
-    public void update() {
+    public void whenGetAll_thenListOfEquipmentsShouldBeReturned() {
+
+        List<Equipment> founds = equipmentService.getAll();
+        verifyFindAllEmployeesIsCalledOnce();
+        assertThat(founds.size()).isEqualTo(equipmentCount);
+        assertThat(founds.get(0).getTitle()).isEqualTo(title);
+        assertThat(founds.get(1).getTitle()).isEqualTo(secondTitle);
+
     }
 
-    @Test
-    public void get() {
+    private void verifyFindByIdIsCalledOnce() {
 
-        String title = "Test";
-        Equipment found = equipmentService.get(1L);
-        assertEquals(found.getTitle(),title);
+        Mockito.verify(equipmentRepository, VerificationModeFactory.times(1)).findById(Mockito.anyLong());
     }
 
-    @Test
-    public void getAll() {
-    }
+    private void verifyFindAllEmployeesIsCalledOnce() {
 
-    @Test
-    public void delete() {
+        Mockito.verify(equipmentRepository, VerificationModeFactory.times(1)).findAll();
     }
-}*/
+}
