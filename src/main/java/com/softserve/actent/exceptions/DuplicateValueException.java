454bed31ae808766a21ed8fde1a6f46d767cc8f4
Senchakmen <senchakevuch@gmail.com>
2019-03-13 21:45:40 +0200

    removed unnecessary exceptions

--- src/main/java/com/softserve/actent/exceptions/DuplicateValueException.java
+++ src/main/java/com/softserve/actent/exceptions/DuplicateValueException.java
@@ -5,12 +5,6 @@ import com.softserve.actent.exceptions.validation.ValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-/**
- * <h2>General exception for duplicate input data</h2>
- * <p>
- * Exception has code: <b>1001</b>
- * </p>
- */
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class DuplicateValueException extends ValidationException {
 
