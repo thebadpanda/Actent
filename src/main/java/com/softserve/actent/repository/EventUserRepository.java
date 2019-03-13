21a8e6e8331b6254edf3b221325a343214812443
RBarnabass <rastafarimood@gmail.com>
2019-03-13 16:22:54 +0200

    Refactoring event user service repository

--- src/main/java/com/softserve/actent/repository/EventUserRepository.java
+++ src/main/java/com/softserve/actent/repository/EventUserRepository.java
@@ -1,10 +1,13 @@
 package com.softserve.actent.repository;
 
+import com.softserve.actent.model.entity.Event;
 import com.softserve.actent.model.entity.EventUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface EventUserRepository extends JpaRepository<EventUser, Long> {
 
+    List<EventUser> findByEvent(Event event);
 }
