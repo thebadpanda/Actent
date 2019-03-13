b2103dc0685b0caaab06128a41828f4be1d33748
onlyVone <43787744+onlyVone@users.noreply.github.com>
2019-03-13 18:17:46 +0200

    Refactor chat(persistance...)

--- src/main/java/com/softserve/actent/model/entity/Chat.java
+++ src/main/java/com/softserve/actent/model/entity/Chat.java
@@ -4,8 +4,18 @@ import com.softserve.actent.constant.StringConstants;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
-
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
