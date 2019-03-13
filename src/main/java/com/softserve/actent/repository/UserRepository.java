bec71706632f1c5945086c9bf3ed302821e69309
Arsen <arsenemarkiv@gmail.com>
2019-03-13 14:33:04 +0200

    changed user repository and service impl

--- src/main/java/com/softserve/actent/repository/UserRepository.java
+++ src/main/java/com/softserve/actent/repository/UserRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findUserByEmail(String email);
+    Optional<User> findUserByEmail(String email);
 
     boolean existsByEmail(String email);
 }
