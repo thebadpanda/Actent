d18a61ae53b8b550cd0a8ff1548b6f15934329f4
Senchakmen <senchakevuch@gmail.com>
2019-03-13 21:45:40 +0200

    removed unnecessary exceptions

--- src/main/java/com/softserve/actent/exceptions/validation/MessageValidationException.java
+++ src/main/java/com/softserve/actent/exceptions/validation/MessageValidationException.java
@@ -4,12 +4,6 @@ import com.softserve.actent.exceptions.codes.ExceptionCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-/**
- * <h2>Exception have to be thrown when message validation failed</h2>
- * <p>
- * All message exception codes has next template: <b>109*</b>
- * </p>
- */
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class MessageValidationException extends IncorrectInputDataException {
     public MessageValidationException(String message, ExceptionCode exceptionCode) {
