dea38f3806c59ef7d36aa08949e15f36f0cdb1fc
Senchakmen <senchakevuch@gmail.com>
2019-03-21 15:20:18 +0200

    add basic configuration for React app

---
+++ src/main/resources/static/src/component/Hallo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Hallo extends React.Component{
+    render() {
+        return (
+                <div>
+                    <h1>Hallo</h1>
+                </div>
+        );
+    }
+}
