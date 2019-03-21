7463d6ef47d3431573724ef6455e17227d97daf4
Arsen <arsenemarkiv@gmail.com>
2019-03-21 21:42:37 +0200

    swagger config

--- src/main/java/com/softserve/actent/config/SwaggerConfiguration.java
+++ src/main/java/com/softserve/actent/config/SwaggerConfiguration.java
@@ -1,5 +1,6 @@
 package com.softserve.actent.config;
 
+import com.google.common.collect.Sets;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -9,6 +10,10 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
 @EnableSwagger2
@@ -21,14 +26,21 @@ public class SwaggerConfiguration {
 
     @Bean
     public Docket decksApi() {
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).pathMapping("/").select()
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .pathMapping("/")
+                .produces(Sets.newHashSet("application/json"))
+                .select()
                 .apis(RequestHandlerSelectors.basePackage("com.softserve.actent"))
                 .paths(PathSelectors.any()).build();
     }
 
     private ApiInfo apiInfo() {
-        return new ApiInfoBuilder().title(TITLE).description(DESCRIPTION).license(LICENSE_TEXT)
-                .version(SWAGGER_API_VERSION).build();
+        return new ApiInfoBuilder().title(TITLE)
+                .description(DESCRIPTION)
+                .license(LICENSE_TEXT)
+                .version(SWAGGER_API_VERSION)
+                .build();
     }
 
 }
