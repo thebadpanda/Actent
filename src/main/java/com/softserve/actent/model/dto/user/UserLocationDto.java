f37e61f11c32996e7a1427bd88bdcf3e88627060
Arsen <arsenemarkiv@gmail.com>
2019-03-26 12:19:27 +0200

    User api logic changed

--- src/main/java/com/softserve/actent/model/dto/user/UserLocationDto.java
+++ src/main/java/com/softserve/actent/model/dto/user/UserLocationDto.java
@@ -8,11 +8,12 @@ import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 
 @Data
-public class UserCityDto {
+public class UserLocationDto {
 
     @Size(max = NumberConstants.USER_ADDRESS_MAX_LENGTH, message = StringConstants.USER_LOCATION_LENGHT_RANGE)
-    private String cityName;
+    private String name;
 
-    @Positive(message = StringConstants.USER_CITY_ID_NEGATIVE_NUMBER)
-    private Long cityId;
+    private String regionName;
+
+    private String regionCountryName;
 }
