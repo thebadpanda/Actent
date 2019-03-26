84777470fe1684ce4c3555f56ae373bfbfd9533b
Arsen <arsenemarkiv@gmail.com>
2019-03-26 12:20:57 +0200

    profile js + css

--- src/main/resources/static/src/component/profile/Profile.js
+++ src/main/resources/static/src/component/profile/Profile.js
@@ -1,60 +1,80 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+import ProfileView from './ProfileView';
 
-const apiUrl = '/api/v1/';
+const apiUrl = 'http://localhost:8080/api/v1';
 
-export default class Profile extends React.Component{
+export const getUserId = () => {
+    let cookies = document.cookie.split(';').map(cookie => cookie.replace(' ', ''));
+    const name = 'user_id=';
 
-    state={
-        isEdit: false,
-        isMyProfile: +this.props.match.params.profileId === getUserId(),
-        firstName : undefined,
-        lastName : undefined,
-        phone : undefined,
-        login : undefined,
-        address : undefined,
-        birthDate : new Date(),
-        bio : undefined,
-        interests : undefined
+    for (let i=0; i < cookies.length; i++) {
+        if(cookies[i].indexOf(name) !== -1) {
+            return +cookies[i].substring(name.length, cookies[i].length);
+        }
+    }
+    return null;
+};
 
-    };
+export default class Profile extends React.Component {
 
-    // TODO: unsafe !!
-    componentWillReceiveProps(nextProps, nextContext) {
-        //TODO: check if
-        this.getProfile(+nextProps.props.match.params.profileId);
+    constructor(props) {
+        super(props);
+        this.state = {
+            userId: 2,
+            isEdit: false,
+            isMyProfile: true,
+                // +this.props.match.params.profileId === getUserId(),
+            firstName: '',
+            lastName: '',
+            phone: '',
+            login: '',
+            address: '',
+            birthDate: new Date().toUTCString(),
+            bio: '',
+            interests: '',
+            avatar: ''
+
+        };
     }
 
+    // // TODO: unsafe !!
+    // componentWillReceiveProps(nextProps, nextContext) {
+    //     //TODO: check if my profile
+    //     this.getProfile();
+    // }
+
     // TODO: unsafe !!
-    componentWillMount() {
-        this.getProfile(this.props.match.params.profileId)
+    componentDidMount() {
+        this.getProfile(event);
     }
 
 
-    handleFirstName = (event) => this.setState({firstName : event.target.value});
+    handleFirstName = (event) => this.setState({firstName: event.target.value});
 
-    handleLastName = (event) => this.setState({lastName : event.target.value});
+    handleLastName = (event) => this.setState({lastName: event.target.value});
 
-    handlePhone = (event) => this.setState({phone : event.target.value});
+    handlePhone = (event) => this.setState({phone: event.target.value});
 
-    handleLogin = (event) => this.setState({login : event.target.value});
+    handleLogin = (event) => this.setState({login: event.target.value});
 
-    handleAddress = (event) => this.setState({address : event.target.value});
+    handleAddress = (event) => this.setState({address: event.target.value});
 
-    handleBirthDate = (event) => this.setState({birthDate : event.target.value});
+    handleBirthDate = (event) => this.setState({birthDate: event.target.value});
 
-    handleBio = (event) => this.setState({bio : event.target.value});
+    handleBio = (event) => this.setState({bio: event.target.value});
 
-    handleInterests = (event) => this.setState({interests : event.target.value});
+    handleInterests = (event) => this.setState({interests: event.target.value});
 
 
-    saveUserSettings = () =>{
+    saveUserSettings = () => {
         //TODO send to backend
     };
 
     isValidName = () => {
-        if (this.state.name && this.state.name.length > 2){
+        if (this.state.name && this.state.name.length > 2) {
             return true;
-        }else{
+        } else {
             return false;
         }
     };
@@ -65,9 +85,64 @@ export default class Profile extends React.Component{
 
 
     getProfile = () => {
-        getProfilesE
-    }
+        // event.preventDefault();
 
+        const profileUrl = apiUrl + "/users/" + this.state.userId;
 
+        axios.get(profileUrl).then(response => {
+
+            console.log(response.data);
+            console.log(response.data['firstName']);
+
+            this.setState({
+                userId: response.data['id'],
+                firstName: response.data['firstName'],
+                lastName: response.data['lastName'],
+                login: response.data['login'],
+                address: response.data['location'],
+                birthDate: response.data['birthDate'],
+                bio: response.data['bio'],
+                interests: response.data['interests'],
+                avatar: response.data['avatar']
+
+            });
+        }).catch(function(error) {
+            console.log(error);
+        });
+    };
+
+    handleEditClick = () => {
+        this.setState({
+            isEdit: false
+        });
+    };
+
+    render() {
+
+        const profileData = {
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            phone: this.state.phone,
+            login: this.state.login,
+            address: this.state.address,
+            birthDate: this.state.birthDate,
+            bio: this.state.bio,
+            interests: this.state.interests,
+            avatar: this.state.avatar
+        };
+
+        const view =
+            (<ProfileView
+                profileData={profileData}
+                isMyProfile={this.state.isMyProfile}
+                onEditClick={this.handleEditClick}
+            />);
+
+        return (
+            <div>
+                {view}
+            </div>
+        )
+    };
 
 }
