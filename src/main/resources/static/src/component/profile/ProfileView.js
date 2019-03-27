e8e202b2b04586e40a13dd1e758a58a2bef25ba5
Arsen <arsenemarkiv@gmail.com>
2019-03-27 13:00:59 +0200

    profile front-end part

--- src/main/resources/static/src/component/profile/ProfileView.js
+++ src/main/resources/static/src/component/profile/ProfileView.js
@@ -9,56 +9,6 @@ function getImageUrl(imageName) {
     }
 }
 import style from './style.css';
-// const styleMain = {
-//     display: 'flex',
-//     flexDirection: 'column',
-//     justifyContent: 'center',
-//     width: '100%',
-//     maxWidth: '700px',
-//     alignItems: 'center',
-//     boxShadow: '0px 0px 31px 2px rgba(0,0,0,0.53)',
-//     margin: '20px auto',
-// };
-//
-// const styleLowerMain1 = {
-//     display: 'flex',
-//     justifyContent: 'center',
-//     width: '100%',
-//     margin: '10px 10px',
-// };
-//
-// const styleLowerMain2 = {
-//     display: 'flex',
-//     justifyContent: 'flex-end',
-//     width: '85%',
-//     margin: '10px 10px',
-// };
-//
-// const styleContainer = {
-//     width: '250px',
-//     height: '250px',
-//     fontSize: '10px',
-//     margin: '10% 5% 5% 5%',
-//     display: 'flex',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-// };
-//
-// const styleInput = {
-//     fontSize: '25px',
-//     fontFamily: 'Roboto, sans-serif',
-// };
-//
-// const styleSpan = {
-//     display: 'inline-block',
-//     fontSize: '15px',
-//     width: '117px',
-// };
-//
-// const imageStyle = {
-//     maxWidth: '100%',
-//     maxHeight: '100%'
-// };
 
 export default class ProfileView extends React.Component {
     constructor(props) {
@@ -81,7 +31,8 @@ export default class ProfileView extends React.Component {
                 <div className= "styleLowerMain1">
 
                     <div className="styleContainer">
-                        <img src={this.props.profileData.avatar && getImageUrl(this.props.profileData.avatar)}
+                        <img src={this.props.profileData.avatar.filePath }
+                        //&& getImageUrl(this.props.profileData.avatar)}
                              alt=""
                              className="imageStyle"
                         />
