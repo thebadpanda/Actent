e70a46b7c94bab50ce03b32627d0c16d7defa438
octap16 <octap17@gmail.com>
2019-03-27 16:10:17 +0200

    add method findAllByChatIdOrderBySendTimeDesc

--- src/main/java/com/softserve/actent/repository/MessageRepository.java
+++ src/main/java/com/softserve/actent/repository/MessageRepository.java
@@ -1,7 +1,6 @@
 package com.softserve.actent.repository;
 
 import com.softserve.actent.model.entity.Message;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,7 +10,7 @@ import java.util.List;
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
-    List<Message> findAllByChatId(Long chatId);
+    List<Message> findAllByChatIdOrderBySendTimeDesc(Long chatId, Pageable pageable);
 
-    Page<Message> findAllByChatId(Long chatId, Pageable pageable);
+    List<Message> findAllByChatId(Long chatId);
 }
