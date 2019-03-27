06c8d161f7ee6c8a6f05b13a44863bed676ea5b6
Arsen <arsenemarkiv@gmail.com>
2019-03-27 12:57:14 +0200

    changes in image dto

--- src/main/java/com/softserve/actent/model/dto/AddImageDto.java
+++ src/main/java/com/softserve/actent/model/dto/AddImageDto.java
@@ -16,8 +16,4 @@ public class AddImageDto {
     @NotBlank(message = ExceptionMessages.IMAGE_NO_FILEPATH)
     private String filePath;
 
-    @NotBlank(message = ExceptionMessages.IMAGE_NO_HASH)
-    @Size(min = NumberConstants.HASH_LENGTH, max = NumberConstants.HASH_LENGTH,
-            message = ExceptionMessages.IMAGE_INAPPROPRIATE_HASH_LENGTH)
-    private String hash;
 }
