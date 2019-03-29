90c8a593c00b493626dd37d0129f13a4b58dfc5c
Arsen <arsenemarkiv@gmail.com>
2019-03-29 10:14:16 +0200

    profile view; edit profile view; send file from front-end to Amazon

--- src/main/resources/static/src/component/profile/ProfileEdit.js
+++ src/main/resources/static/src/component/profile/ProfileEdit.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import axios from 'axios';
 import DatePicker from 'material-ui/DatePicker';
-import apiUrl from './Profile.js';
+import {apiUrl} from './Profile.js';
+import {getImageUrl} from './ProfileView';
+import {imageValidator} from './FileUploadValidator';
+import {Card} from 'material-ui/Card';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import FileUpload from './FileUpload'
+import Typography from "@material-ui/core/Typography";
+import styles from './style.css'
 
 
+export default class ProfileEdit extends React.Component {
 
-
-export default class ProfileEdit extends React.Component{
-
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             userId: this.props.profileData.userId,
@@ -17,10 +23,13 @@ export default class ProfileEdit extends React.Component{
             phone: this.props.profileData.phone,
             login: this.props.profileData.login,
             address: this.props.profileData.address,
-            birthDate: new Date(this.props.profileData.birthDate),
+            birthday: new Date(this.props.profileData.birthday),
             bio: this.props.profileData.bio,
             interests: this.props.profileData.interests,
-            avatar: this.props.profileData.avatar
+            // imageUrl: getImageUrl(this.props.profileData.avatar.filePath),
+            imageUrl: this.props.profileData.avatar.filePath,
+            imageName: this.props.profileData.avatar.filePath,
+            imageData: {}
         };
     }
 
@@ -34,26 +43,28 @@ export default class ProfileEdit extends React.Component{
 
     handleAddress = (event) => this.setState({address: event.target.value});
 
-    handleBirthDate = (event, date) => this.setState({birthDate: date});
+    handleBirthday = (event, date) => this.setState({birthday: date});
 
     handleBio = (event) => this.setState({bio: event.target.value});
 
     handleInterests = (event) => this.setState({interests: event.target.value});
 
-    getBirthDate = () => {
-        return this.state.birthDate.getFullYear() + '-' +
-        this.state.birthDate.getMonth()+ '-' +
-            this.state.birthDate.getDate();
+    getBirthday = () => {
+        return this.state.birthday.getFullYear() + '-' +
+            this.state.birthday.getMonth() + '-' +
+            this.state.birthday.getDate();
     };
     saveUserSettings = () => {
-        const url = apiUrl + /users/ + this.props.userId;
+        console.log("state: " + this.state.userId + "props: " + this.props.userId);
 
-        axios.put( url, {
-            firstName : this.state.firstName,
+        const url = apiUrl + /users/ + this.state.userId;
+
+        axios.put(url, {
+            firstName: this.state.firstName,
             lastName: this.state.lastName,
             login: this.state.login,
             address: this.state.address,
-            birthDate: this.getBirthDate(),
+            birthday: this.getBirthday(),
             bio: this.state.bio,
             interests: this.state.interests,
         }).then(() => {
@@ -63,7 +74,35 @@ export default class ProfileEdit extends React.Component{
     };
 
     saveUserPhoto = () => {
+        const uploadUrl = apiUrl + '/storage/uploadFile/';
+        const userUrl = apiUrl + /users/ + this.state.userId;
+        const requestTimeout = 30000;
 
+        axios.post(uploadUrl, this.state.imageData, {
+            timeout: requestTimeout,
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }})
+            .then(response => {
+                this.setState({
+                    imageName: response.data['image_key']
+                });
+            }).then(() => {
+            axios.post(userUrl, this.state.imageName)
+                .then(response => {
+                    this.props.onCloseClick();
+                });
+            // this.hideLinearProgress();
+        }).catch(error => {
+            console.log(error);
+        });
+    };
+
+    fetchData = (imageData, imageUrl) => {
+        this.setState({
+            imageData: imageData,
+            imageUrl: imageUrl
+        }, () => { this.saveUserPhoto(); });
     };
 
     isValidName = () => {
@@ -77,4 +116,99 @@ export default class ProfileEdit extends React.Component{
     isValidEmail = () => {
         return !!this.state.email;
     };
+
+    render() {
+        return (
+            <div className="styleMainDiv">
+                <Card className="styleCard">
+                    <Typography component="h5" variant="h5" className="styleHeader" align="center">
+                        Edit profile
+                    </Typography>
+
+                    <div className="styleContainerEdit">
+                        <img src={this.state.imageUrl}
+                             alt=""
+                             className="imageStyle"
+                        />
+                    </div>
+                    <FileUpload
+                        fetchData={this.fetchData}
+                        handleSavePhoto={this.handleSavePhoto}
+                    />
+                    <div className="styleName">
+                        <TextField
+                            id="tv_first_name"
+                            onChange={this.handleFirstName}
+                            label="First Name:"
+                            value={this.state.firstName}
+                            fullWidth={true}
+                        />
+                        <TextField
+                            id="tv_last_name"
+                            onChange={this.handleLastName}
+                            label="Last Name"
+                            value={this.state.lastName}
+                            fullWidth={true}
+                        />
+                        <TextField
+                            id="tv_login"
+                            label='Login'
+                            onChange={this.handleLogin}
+                            fullWidth={true}
+                            rowsMax={3}
+                            value={this.state.login}
+                        />
+                        <TextField
+                            id="tv_address"
+                            label='Address'
+                            onChange={this.handleAddress}
+                            fullWidth={true}
+                            multiline
+                            rowsMax={3}
+                            value={this.state.address.name}
+                        />
+                        <DatePicker
+                            id="date_picker_birthday"
+                            label="Birthday"
+                            mode="landscape"
+                            openToYearSelection={true}
+                            fullWidth={true}
+                            onChange={this.handleBirthday}
+                            value={this.state.birthday}
+
+                        />
+                        <TextField
+                            id="tv_bio"
+                            label='Bio'
+                            onChange={this.handleBio}
+                            fullWidth={true}
+                            multiline
+                            rowsMax={3}
+                            value={this.state.bio}
+                        />
+                        {/*<ChangePassword />*/}
+                    </div>
+                    <div className="styleButtons">
+                        <Button
+                            label="Cancel"
+                            variant="contained"
+                            color="secondary"
+                            onClick={this.props.onCloseClick}
+                        >Cancel </Button>
+                        <Button
+                            label="Save"
+                            color="primary"
+                            variant="contained"
+                            onClick={this.saveUserSettings}
+                        >Save </Button>
+                    </div>
+                    {/*<div className="linearProgressWrapperStyle">*/}
+                    {/*<LinearProgress mode='indeterminate' className= "linearProgressStyle"/>*/}
+                    {/*</div>*/}
+                </Card>
+            </div>
+        )
+    }
+
+
 }
