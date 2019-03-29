aa00d6a7b2be2fb4ff92e9f075c2f71253e023cb
Ira <ira.shturyn@gmail.com>
2019-03-29 09:49:34 +0200

    delete unused constants, add urlConstants

--- src/main/java/com/softserve/actent/constant/UrlConstants.java
+++ src/main/java/com/softserve/actent/constant/UrlConstants.java
@@ -2,4 +2,9 @@ package com.softserve.actent.constant;
 
 public class UrlConstants {
     public static final String API_V1 = "/api/v1";
+
+    public static final String API_V1_COUNTRIES = "/api/v1/countries";
+    public static final String API_V1_REGIONS = "/api/v1/regions";
+    public static final String API_V1_CITIES = "/api/v1/cities";
+    public static final String API_V1_LOCATIONS = "/api/v1/locations";
 }
