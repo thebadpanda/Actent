5e6ec82ca61f94aaf1109289e5dfd2a6d4d44d50
Oleh Volchkov <volchkov.oleh@gmail.com>
2019-03-30 16:30:55 +0200

    moved old react components here

---
+++ actent-app-client/src/component/Hallo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Hallo extends React.Component{
+    render() {
+        return (
+                <div>
+                    <h1>Hallo</h1>
+                    <h3>Actent is ready</h3>
+                </div>
+        );
+    }
+}
