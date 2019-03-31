bc4d2c807d336c88ba1db53b7fcd8145f101359f
Oleh Volchkov <volchkov.oleh@gmail.com>
2019-04-01 02:28:06 +0300

    constants: api endpoints, local storage token key and auth header

---
+++ actent-app-client/src/constants/apiConstants.js
@@ -0,0 +1,8 @@
+export const ACCESS_TOKEN = 'accessToken';
+export const AUTHORIZATION_HEADER = 'Authorization';
+export const BEARER = 'Bearer';
+
+export const API_BASE_URL = 'http://localhost:8080/api/v1';
+export const API_USERS_URL = '/users';
+export const API_REVIEWS_URL = '/reviews';
+export const API_AUTH_URL = '/auth';
