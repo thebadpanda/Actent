44caed0789e58cb809fc25fb71287979caf491f2
Arsen <arsenemarkiv@gmail.com>
2019-04-01 20:04:07 +0300

    profile front-end part

--- src/main/resources/static/src/component/profile/ProfileView.js
+++ src/main/resources/static/src/component/profile/ProfileView.js
@@ -27,24 +27,33 @@ export default class ProfileView extends React.Component {
             </Button>) :
             null;
 
+        const img = this.props.profileData.avatar !== null ?
+            (<img src={getImageUrl(this.props.profileData.avatar.filePath)}
+                  alt=""
+                  className="imageStyle"
+                />
+            ):
+            (<img src={"https://s3.ap-south-1.amazonaws.com/actent-res/1554136129708-default-user.png"}
+                  alt=""
+                  className="imageStyle"
+                />
+            );
+
         return (
+
             <div className = "styleMain">
                 <div className= "styleLowerMain1">
 
                     <div className="styleContainer">
-                        <img src={this.props.profileData.avatar.filePath }
-                        //&& getImageUrl(this.props.profileData.avatar)}
-                             alt=""
-                             className="imageStyle"
-                        />
+                        {img}
                     </div>
 
                     <div>
                         <p className="styleInput"><span className="styleSpan">First Name:</span>{this.props.profileData.firstName}</p>
                         <p className="styleInput"><span className="styleSpan">Last Name:</span>{this.props.profileData.lastName}</p>
                         <p className="styleInput"><span className="styleSpan">Login:</span>{this.props.profileData.login}</p>
-                        <p className="styleInput"><span className="styleSpan">Address:</span>{this.props.profileData.address.name},
-                            {this.props.profileData.address.regionCountryName}</p>
+                        <p className="styleInput"><span className="styleSpan">Address:</span>{this.props.profileData.address.name || null},
+                            {this.props.profileData.address.regionCountryName || null}</p>
                         <p className="styleInput"><span className="styleSpan">Birth Date:</span>{this.props.profileData.birthday}</p>
                         <p className="styleInput"><span className="styleSpan">Bio:</span>{this.props.profileData.bio}</p>
                         <p className="styleInput"><span className="styleSpan">Interests:</span>{this.props.profileData.interests}</p>
