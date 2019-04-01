313b6af7a2096011c8e1eac9dc68aa645833f102
octap16 <octap17@gmail.com>
2019-04-01 21:54:15 +0300

    create findAllByChatId methods

--- src/main/java/com/softserve/actent/service/MessageService.java
+++ src/main/java/com/softserve/actent/service/MessageService.java
@@ -1,13 +1,16 @@
 package com.softserve.actent.service;
 
 import com.softserve.actent.model.entity.Message;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
-public interface MessageService extends BaseCrudService<Message>{
+public interface MessageService extends BaseCrudService<Message> {
 
     Message addImageMessage(Message message);
 
-    List<Message> getNextThirtyMessagesByChatId(Long chatId, int pageNumber);
+    List<Message> getCurrentMessagesByChatId(Long chatId, Pageable pageable);
+
+    List<Message> getMessagesByChatId(Long chatId);
 
 }
