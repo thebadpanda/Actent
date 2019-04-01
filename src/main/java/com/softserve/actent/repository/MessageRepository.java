f062afb2ccf2533be0c94dcae3a64e8c72c3a942
octap16 <octap17@gmail.com>
2019-04-01 21:53:13 +0300

    create findAllByChatId

--- src/main/java/com/softserve/actent/repository/MessageRepository.java
+++ src/main/java/com/softserve/actent/repository/MessageRepository.java
@@ -1,6 +1,7 @@
 package com.softserve.actent.repository;
 
 import com.softserve.actent.model.entity.Message;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,7 +11,7 @@ import java.util.List;
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
-    List<Message> findAllByChatIdOrderBySendTimeDesc(Long chatId, Pageable pageable);
+    Page<Message> findAllByChatId(Long chatId, Pageable pageable);
 
     List<Message> findAllByChatId(Long chatId);
 }
