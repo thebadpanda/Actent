e9b2a50839b47cf4554ca79eb976cb2373bcfafb
onlyVone <43787744+onlyVone@users.noreply.github.com>
2019-04-02 11:57:51 +0300

    Add login and registration with security logic. Add axios mehods for signin/signup user

--- src/main/resources/static/src/util/apiUtils.js
+++ src/main/resources/static/src/util/apiUtils.js
@@ -5,6 +5,9 @@ import {
     BEARER,
     API_USERS_URL,
     API_REVIEWS_URL,
+    API_SIGN_IN_URL,
+    API_AUTH_URL,
+    API_SIGN_UP_URL,
 } from '../constants/apiConstants';
 import axios from 'axios';
 
@@ -21,22 +24,33 @@ export const testConfiguration = _ => {
 export const confidureAxios = _ => {
     axios.defaults.baseURL = API_BASE_URL;
     axios.defaults.headers.common['Content-Type'] = 'application/json';
+    axios.defaults.headers.common[AUTHORIZATION_HEADER] = `${BEARER} ${localStorage.getItem(ACCESS_TOKEN)}`;
 };
 
-export const saveAuthorizationToken = accessToken => localStorage.setItem(ACCESS_TOKEN, accessToken);
+export const saveAuthorizationToken = accessToken => {
+    localStorage.setItem(ACCESS_TOKEN, accessToken);
+    setAuthorizationHeader();
+}
 
 export const removeAuthorizationToken = _ => localStorage.removeItem(ACCESS_TOKEN);
 
-export const setAuthorizationHeader = _ =>
+export const setAuthorizationHeader = _ => {
+
+    console.log(localStorage.getItem(ACCESS_TOKEN));
+    console.log(`${BEARER} ${localStorage.getItem(ACCESS_TOKEN)}`);
+
     localStorage.getItem(ACCESS_TOKEN)
         ? (axios.defaults.headers.common[AUTHORIZATION_HEADER] = `${BEARER} ${localStorage.getItem(ACCESS_TOKEN)}`)
         : Promise.reject('No access token set.');
+
+    console.log(axios.defaults.headers.common[AUTHORIZATION_HEADER]);
+}
 
 export const removeAuthorizationHeader = _ => delete axios.defaults.headers.common[AUTHORIZATION_HEADER];
 
 export const addReviewToUser = async (review, userId) => {
     if (!axios.defaults.headers.common[AUTHORIZATION_HEADER]) {
-        return Promise.reject('No access token set.');
+        return Promise.reject('Review: No access token set.');
     }
 
     if (!review.text || !review.score || !userId) {
@@ -58,6 +72,10 @@ export const getAllUserReviews = async userId =>
         : Promise.reject('No access token set.');
 
 export const getUserById = async userId =>
-    checkTokenHeader() ? axios.get(`${API_USERS_URL}/${userId}`) : Promise.reject('No access token set.');
+    checkTokenHeader() ? axios.get(`${API_USERS_URL}/${userId}`) : Promise.reject('getUserById: No access token set.');
 
 export const checkTokenHeader = _ => (axios.defaults.headers.common[AUTHORIZATION_HEADER] ? true : false);
+
+export const getTokenFromCredentials = async userCredentials => axios.post(API_AUTH_URL + API_SIGN_IN_URL, userCredentials);
+
+export const registerUser = async userData => axios.post(API_AUTH_URL + API_SIGN_UP_URL, userData);
