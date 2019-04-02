bb76262548a483be66b520c712c8de2564be8d3a
onlyVone <43787744+onlyVone@users.noreply.github.com>
2019-04-02 12:00:26 +0300

    Change host port to 8080

--- src/main/resources/static/src/constants/apiConstants.js
+++ src/main/resources/static/src/constants/apiConstants.js
@@ -2,7 +2,7 @@ export const ACCESS_TOKEN = 'accessToken';
 export const AUTHORIZATION_HEADER = 'Authorization';
 export const BEARER = 'Bearer';
 
-export const API_BASE_URL = 'http://localhost/api/v1';
+export const API_BASE_URL = 'http://localhost:8080/api/v1';
 export const API_USERS_URL = '/users';
 export const API_REVIEWS_URL = '/reviews';
 export const API_AUTH_URL = '/auth';
