c36b4707d3c50dd7e66e4c624eeebc69af25c2ac
Arsen <arsenemarkiv@gmail.com>
2019-04-04 01:52:58 +0300

    saving profile's life

--- actent-app-client/src/index.js
+++ actent-app-client/src/index.js
@@ -4,10 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+
+const theme = createMuiTheme({
+    typography: {
+        useNextVariants: true,
+    },
+});
 
 ReactDOM.render(
     <Router>
-        <App />
+        <MuiThemeProvider theme={theme}>
+            <App />
+        </MuiThemeProvider>
     </Router>,
     document.getElementById('root'),
 );
