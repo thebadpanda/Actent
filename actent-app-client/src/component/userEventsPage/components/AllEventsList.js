242e50823d7f9328585e8f31d03d7da2059a2211
Ira <ira.shturyn@gmail.com>
2019-04-04 13:58:56 +0300

    add link to button

--- actent-app-client/src/component/userEventsPage/components/AllEventsList.js
+++ actent-app-client/src/component/userEventsPage/components/AllEventsList.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Event from './Event';
 
 export default class PastEventList extends React.Component {
-  render() {
-    return (
-      <div>
-        {this.props.events.map(event => (
-          <Event key={event.eventId.toString()} title={event.eventTitle} description={event.eventDescription} />
-        ))}
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                {this.props.events.map(event => (
+                    <Event key={event.eventId.toString()} eventId={event.eventId} userId={event.userId}
+                           title={event.eventTitle}
+                           description={event.eventDescription}/>
+                ))}
+            </div>
+        );
+    }
 }
