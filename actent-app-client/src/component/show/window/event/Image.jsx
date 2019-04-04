73d93a2ddaebc23aac2d2880ba8c1220b4f72211
Barnabass <rastafarimood@gmail.com>
2019-04-04 12:44:22 +0300

    refactor

--- actent-app-client/src/component/show/window/event/Image.jsx
+++ actent-app-client/src/component/show/window/event/Image.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { el } from 'date-fns/esm/locale';
 
 class Image extends React.Component {
 
     render() {
 
-        return (
-            <div>
-                <h1>{this.props.image}</h1>
-            </div>
-        );
+        if  (this.props.image) {
+            return (
+                <div>
+                    <h1>{this.props.image}</h1>
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <img src='bigstock-events-7444309.jpg' />
+                </div>
+            )
+        }
     }
 }
 
