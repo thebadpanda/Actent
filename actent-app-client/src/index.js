7500e9649c6932dba76329a87cc93d21c4b1aa00
Arsen <arsenemarkiv@gmail.com>
2019-04-04 13:56:24 +0300

    p[repare to merge

--- actent-app-client/src/index.js
+++ actent-app-client/src/index.js
@@ -5,13 +5,6 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-// import createMuiTheme from 'material-ui/styles/createMuiTheme';
-
-// const theme = createMuiTheme({
-//     typography: {
-//         useNextVariants: true,
-//     },
-// });
 
 ReactDOM.render(
     <Router>
